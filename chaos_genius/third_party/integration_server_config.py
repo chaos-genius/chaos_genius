8e68d3248fca1c8176f1e6d54badb00e49acf475
Manas Solanki <manassolanki@gmail.com>
2021-07-10 09:30:55 +0530

    restructure the integration codebase

--- chaos_genius/third_party/integration_server_config.py
+++ chaos_genius/third_party/integration_server_config.py
@@ -1,4 +1,14 @@
 
+# TODO: This is the default worksapce
+# Check this link for more information on the same
+# https://github.com/airbytehq/airbyte/blob/07a45df4547fa5f12a170dff0bfc56c39c3d2e9f/airbyte-config/persistence/src/main/java/io/airbyte/config/persistence/PersistenceConstants.java
+DEFAULT_WORKSPACE_ID = "5ae6b09b-fdec-41af-aaf7-7d94cfc33ef6"
+
+# Destination for the third party data
+DESTINATION_DEF_ID = "25c5221d-dce2-4163-ade9-739ef790f503" # POSTGRES DB
+DESTINATION_TYPE = "postgresql"
+
+# Mapping configuration from the source to database
 SOURCE_CONFIG_MAPPING = {
     "71607ba1-c0ac-4799-8049-7f4b90dd50f7": {
         "syncMode": "full_refresh",
@@ -19,9 +29,6 @@ SOURCE_WHITELIST_AND_TYPE = {
     # "e094cb9a-26de-4645-8761-65c0c425d1de", # Stripe
     # "29b409d9-30a5-4cc8-ad50-886eb846fea3", # Quickbooks
 }
-
-DESTINATION_DEF_ID = "25c5221d-dce2-4163-ade9-739ef790f503" # POSTGRES DB
-DESTINATION_TYPE = "postgresql"
 
 # KEY: source configuration for creating the database connection
 # Value: corresponding database key in integration configuration

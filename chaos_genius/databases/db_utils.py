7654393240e628b587985cad49af40815373e1a7
Manas Solanki <manassolanki@gmail.com>
2021-07-13 16:28:29 +0530

    fix: minor fixes - fix the integration server constant name - add the todo

--- chaos_genius/databases/db_utils.py
+++ chaos_genius/databases/db_utils.py
@@ -2,7 +2,7 @@
 
 def create_sqlalchemy_uri(db_type, host, port, database, username, password):
     db_uri = ""
-    if db_type == "postgresql":
+    if db_type == "postgres":
         db_uri = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
     elif db_type == "mysql":
         db_uri = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"

ad3a5b6e861539abc65b765ebb008ab33bd070aa
Manas Solanki <manassolanki@gmail.com>
2021-07-29 11:23:24 +0530

    feat: add api for alert testing

--- chaos_genius/views/config_setting_view.py
+++ chaos_genius/views/config_setting_view.py
@@ -12,6 +12,8 @@ from flask import (
 )
 from chaos_genius.databases.models.data_source_model import DataSource
 from chaos_genius.databases.models.kpi_model import Kpi
+from chaos_genius.alerts.slack import trigger_overall_kpi_stats
+from chaos_genius.views.kpi_view import kpi_aggregation
 
 blueprint = Blueprint("config_settings", __name__)
 
@@ -54,3 +56,29 @@ def get_onboarding_status():
     ]
     completion_precentage = int(len([step for step in steps if step["step_done"]])/len(steps)*100)
     return jsonify({'data': {"steps": steps, "completion_precentage": completion_precentage}})
+
+
+@blueprint.route("/test-alert", methods=["POST"])
+def test_alert():
+    """Test Alert."""
+    if request.is_json:
+        data = request.get_json()
+        kpi_info = Kpi.get_by_id(data["kpiId"]).as_dict
+        connection_info = DataSource.get_by_id(kpi_info["data_source"]).as_dict
+        kpi_aggregation_stats = kpi_aggregation(kpi_info, connection_info, 'wow')
+        overall_stats = {
+            "current": kpi_aggregation_stats["panel_metrics"]["grp2_metrics"],
+            "past": kpi_aggregation_stats["panel_metrics"]["grp1_metrics"],
+            "impact": kpi_aggregation_stats["panel_metrics"]["impact"]
+        }
+        status = trigger_overall_kpi_stats(
+            data["name"],
+            kpi_info["name"],
+            connection_info["name"],
+            data["alertMessage"],
+            overall_stats
+        )
+        return jsonify({"message": f"Alert has been tested successfully.", "status": status})
+    else:
+        return jsonify({"error": "The request payload is not in JSON format"})
+

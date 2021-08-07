d3f7a76ce9ec2900eb292ae1fc83b36a2988d15b
Daj Katal <danykattouf@gemsdaa.net>
2021-08-07 17:59:12 +0400

    Added files for Metabase export to JSON

---
+++ sandbox/core/metabase-export/run_metabase_export.py
@@ -0,0 +1,14 @@
+from metabase_export import MetabaseExport
+
+metabase_db_credentials = {
+    "database_type": "postgresql",
+    "database_driver": "psycopg2",
+    "user": "postgres",
+    "pass": "password",
+    "host": "localhost",
+    "port": "5432",
+    "database": "metabase_imported_data"
+}
+
+x = MetabaseExport(metabase_db_credentials)
+x.export()

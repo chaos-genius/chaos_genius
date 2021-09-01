1566c3e89bf032e59c6356a26c637744ab981dc1
kartikay-bagla <kartikaybagla@gmail.com>
2021-09-01 09:49:22 +0530

    fix (db): alembic now compares types of columns

--- migrations/env.py
+++ migrations/env.py
@@ -45,7 +45,7 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True
+        url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True
     )
 
     with context.begin_transaction():
@@ -76,6 +76,7 @@ def run_migrations_online():
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
+            compare_type=True,
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )

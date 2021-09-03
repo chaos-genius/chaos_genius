bd5c33d4d941542d8bf2103340c8bf009774b24a
Moghan Kumar <moghan@sketchbrahma.com>
2021-09-03 11:56:18 +0530

    activate analytics

--- frontend-new/src/components/Analystics/index.js
+++ frontend-new/src/components/Analystics/index.js
@@ -1,64 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './analystics.scss';
 
 const Analystics = () => {
-  const [tab, setTab] = useState('rca');
   return (
     <>
       <div className="dashboard-subheader">
         <div className="common-tab">
           <ul>
-            <li
-              className={tab === 'rca' ? 'active' : ''}
-              onClick={() => setTab('rca')}>
-              Configure AutoRCA
-            </li>
-            <li
-              className={tab === 'anomoly' ? 'active' : ''}
-              // onClick={() => setTab('anomoly')}
-            >
-              Configure Anomoly Detector
-            </li>
+            <li>Configure Anomoly Detector for Selected KPI</li>
           </ul>
         </div>
       </div>
       <div className="form-container">
         <div className="form-group">
-          <label>Select Time Window </label>
-          <input type="text" className="form-control" />
+          <label>Time Window</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="90 Days"
+            disabled
+          />
         </div>
         <div className="form-group">
-          <label>Frequency </label>
-          <input type="text" className="form-control" />
+          <label>Model Frequency</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Daily"
+            disabled
+          />
         </div>
-        {tab === 'anomoly' ? (
-          <div className="form-group">
-            <label>Baseline</label>
-            <input type="text" className="form-control" />
-          </div>
-        ) : null}
-        {/* <div className="form-group">
-          <label>Dimension level (max upto 3)</label>
-          <input type="text" className="form-control" />
-        </div> */}
-        {tab === 'anomoly' ? (
-          <div className="form-check check-box">
-            <input className="form-check-input" type="checkbox" />
-            <label className="form-check-label">Use historical data</label>
-          </div>
-        ) : null}
-        {/* <div className="form-check check-box">
-          <input className="form-check-input" type="checkbox" />
-          <label className="form-check-label">
-            Use as default setting for all KPI’s
-          </label>
-        </div>
-        <div className="form-action">
-          <button className="btn black-button" disabled>
-            <span>Set Up</span>
-          </button>
-        </div> */}
       </div>
     </>
   );

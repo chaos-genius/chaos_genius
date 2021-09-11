37a8ee56bb0dcd36ac3ff11fd2677c51bc3cdf7c
Fletchersan <amoghdhardiwan@gmail.com>
2021-09-11 11:13:36 +0530

    fix(anomaly): added appropriate sensitivity values to StdDeviModel and EWSTDModel

--- chaos_genius/core/anomaly/models/stddevi_model.py
+++ chaos_genius/core/anomaly/models/stddevi_model.py
@@ -2,11 +2,10 @@ import pandas as pd
 
 from chaos_genius.core.anomaly.models import AnomalyModel
 
-#FIXME: add accurate threshold values
 STDSENS = {
-    'high': 0.75,
-    'medium': 1.25,
-    'low': 2
+    'high': 0.8,
+    'medium': 0.9,
+    'low': 0.95
 }
 
 class StdDeviModel(AnomalyModel):

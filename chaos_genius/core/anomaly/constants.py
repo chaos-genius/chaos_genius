6d2d4cffde95689265d5f0a99e74a82b467c93a7
Amatullah <amatullahsethji@gmail.com>
2021-09-16 10:04:52 +0400

    feat(anomaly) : added ETS Model

--- chaos_genius/core/anomaly/constants.py
+++ chaos_genius/core/anomaly/constants.py
@@ -3,13 +3,16 @@ from chaos_genius.core.anomaly.models import ProphetModel
 from chaos_genius.core.anomaly.models import EWSTDModel
 from chaos_genius.core.anomaly.models import NeuralProphetModel
 from chaos_genius.core.anomaly.models import GreyKiteModel
+from chaos_genius.core.anomaly.models import ExpTSModel
+
 
 MODEL_MAPPER = {
     "StdDeviModel": StdDeviModel,
     "ProphetModel": ProphetModel,
     "EWSTDModel": EWSTDModel,
     "NeuralProphetModel": NeuralProphetModel,
-    "GreyKiteModel": GreyKiteModel
+    "GreyKiteModel": GreyKiteModel,
+    "ETSModel" : ExpTSModel
 }
 
 FREQUENCY_DELTA = {

8be058c0b98f45511a04a4dbd12196259e52ab7e
NaikAayush <ayush@pesu.pes.edu>
2021-09-18 16:16:38 +0530

    refactor: change SQLALCHEMY_DATABASE_URI source, as duplicate variable exists

--- chaos_genius/settings.py
+++ chaos_genius/settings.py
@@ -15,7 +15,7 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL_CG_DB")
 SECRET_KEY = env.str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)

ce97c35fe46797aa7dfbc31cb5f4701c73865f02
Moghan Kumar <moghan@sketchbrahma.com>
2021-09-21 12:05:02 +0530

    fix: alert table action for edit and delete

--- frontend-new/src/components/AlertTable/index.jsx
+++ frontend-new/src/components/AlertTable/index.jsx
@@ -9,12 +9,17 @@ import Datadog from '../../assets/images/table/datadog.svg';
 import Rectangle from '../../assets/images/table/downarrow.svg';
 import Email from '../../assets/images/table/gmail.svg';
 import Noresult from '../Noresult';
+import More from '../../assets/images/more.svg';
+import Moreactive from '../../assets/images/more-active.svg';
+import Edit from '../../assets/images/edit.svg';
+import EditActive from '../../assets/images/datasourceedit-active.svg';
+import DeleteActive from '../../assets/images/delete-active.svg';
+
 import { formatDate } from '../../utils/date-helper';
 
 const AlertTable = ({ alertData, alertSearch }) => {
   return (
     <div className="table-responsive">
-      {' '}
       <table className="table">
         <thead>
           <tr>
@@ -26,6 +31,7 @@ const AlertTable = ({ alertData, alertSearch }) => {
               Triggered <img src={Rectangle} alt="down arrow" />
             </th>
             <th>Channel</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -70,6 +76,51 @@ const AlertTable = ({ alertData, alertSearch }) => {
                           alt={alert.alert_channel}
                         />
                       </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="more-dropdown">
+                      <div
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        aria-haspopup="true">
+                        <img
+                          src={More}
+                          alt="More"
+                          className="moreoption"
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="bottom"
+                          title="Actions"
+                        />
+                        <img
+                          src={Moreactive}
+                          alt="More"
+                          className="moreoption-active"
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="bottom"
+                          title="Actions"
+                        />
+                      </div>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <img
+                            src={Edit}
+                            alt="Edit"
+                            className="action-disable"
+                          />
+                          <img
+                            src={EditActive}
+                            alt="Edit"
+                            className="action-active"
+                          />
+                          Edit
+                        </li>
+
+                        <li className="delete-item" onClick={() => {}}>
+                          <img src={DeleteActive} alt="Delete" />
+                          Delete
+                        </li>
+                      </ul>
                     </div>
                   </td>
                 </tr>

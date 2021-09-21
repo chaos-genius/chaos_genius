b2b0e26f6f28a7a2ea582dd7906336cb71057530
santhosh kumar <santhosh@sketchbrama.com>
2021-09-21 18:56:54 +0530

    fix: home page search and dates changes done

--- frontend-new/src/components/Kpihome/index.jsx
+++ frontend-new/src/components/Kpihome/index.jsx
@@ -15,6 +15,7 @@ import highchartsMore from 'highcharts/highcharts-more';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getHomeKpi } from '../../redux/actions';
+import Fuse from 'fuse.js';
 
 highchartsMore(Highcharts);
 
@@ -40,7 +41,8 @@ const Kpihome = () => {
   const { homeKpiData, homeKpiLoading } = useSelector(
     (state) => state.onboarding
   );
-
+  const [kpiHomeData, setKpiHomeData] = useState([]);
+  const [search, setSearch] = useState('');
   const [timeline, setTimeLine] = useState({
     value: 'wow',
     label: 'Current Week on Last Week'
@@ -112,6 +114,34 @@ const Kpihome = () => {
     };
   };
 
+  useEffect(() => {
+    if (search !== '') {
+      searchKpi();
+    } else {
+      setKpiHomeData(homeKpiData);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search, homeKpiData]);
+
+  const searchKpi = () => {
+    if (search !== '') {
+      const options = {
+        keys: ['name']
+      };
+
+      const fuse = new Fuse(homeKpiData, options);
+
+      const result = fuse.search(search);
+      setKpiHomeData(
+        result.map((item) => {
+          return item.item;
+        })
+      );
+    } else {
+      setKpiHomeData(homeKpiData);
+    }
+  };
+
   if (homeKpiLoading) {
     return (
       <div className="load loader-page">
@@ -140,6 +170,7 @@ const Kpihome = () => {
                   type="text"
                   className="form-control h-40"
                   placeholder="Search KPI"
+                  onChange={(e) => setSearch(e.target.value)}
                 />
                 <span>
                   <img src={Search} alt="Search Icon" />
@@ -155,9 +186,9 @@ const Kpihome = () => {
               />
             </div>
           </div>
-          {homeKpiData &&
-            homeKpiData.length !== 0 &&
-            homeKpiData.map((item) => {
+          {kpiHomeData &&
+            kpiHomeData.length !== 0 &&
+            kpiHomeData.map((item) => {
               return (
                 <div className="kpi-card" key={item.id}>
                   <div className="kpi-content kpi-content-label">
@@ -165,11 +196,23 @@ const Kpihome = () => {
                     <label>(Mins)</label>
                   </div>
                   <div className="kpi-content">
-                    <label>This Week</label>
+                    <label>
+                      {timeline.value === 'wow'
+                        ? 'This Week'
+                        : timeline.value === 'mom'
+                        ? 'This Month'
+                        : 'This Day'}
+                    </label>
                     <span>{item.current}</span>
                   </div>
                   <div className="kpi-content">
-                    <label>Previous Week</label>
+                    <label>
+                      {timeline.value === 'wow'
+                        ? 'Previous Week'
+                        : timeline.value === 'mom'
+                        ? 'Previous Month'
+                        : 'Previous Day'}
+                    </label>
                     <span>{item.prev}</span>
                   </div>
                   <div className="kpi-content">
@@ -186,7 +229,14 @@ const Kpihome = () => {
                     <label>Anomalies</label>
                     <span>
                       {item.anomaly_count}
-                      <label className="anomalies-period">(last week)</label>
+                      <label className="anomalies-period">
+                        {' '}
+                        {timeline.value === 'wow'
+                          ? '(last week)'
+                          : timeline.value === 'mom'
+                          ? '(last month)'
+                          : ' (last day)'}{' '}
+                      </label>
                     </span>
                   </div>
                   <div className="kpi-content kpi-graph">

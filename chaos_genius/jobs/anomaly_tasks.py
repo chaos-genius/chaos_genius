341e40339847420c92be1d54d4ac5252f8191a3a
Samyak S Sarnayak <samyak201@gmail.com>
2021-09-22 20:43:20 +0530

    feat(celery): new scheduler that separates anomaly and RCA
     - RCA is run even if anomaly is not configured
     - RCA uses "rca_time" in scheduler_params
        - or kpi.created_at if that isn't set
     - if anomaly is setup, both anomaly and RCA run at the same time
        - configured as "time" in scheduler_params
     - anomaly is configured if "model_name" is set in anomaly_params
     - TODO: set rca_time at KPI setup time
     - TODO: run RCA task at KPI setup
     - TODO: run anomaly task at anomaly setup

--- chaos_genius/jobs/anomaly_tasks.py
+++ chaos_genius/jobs/anomaly_tasks.py
@@ -85,21 +85,24 @@ def anomaly_kpi():
     return res
 
 
-# runs every hour
-# if hour > scheduled hour, run task
-# last_scheduled_time -> if it's < specified hour of today's date, run task
+# runs every N time (set in celery_config)
+# if time > scheduled time today, run task
+# last_scheduled_time -> if it's < specified time of today's date, run task
 @celery.task
 def anomaly_scheduler():
     # find KPIs
     kpis: Kpi = Kpi.query.distinct("kpi_id").filter(
         (Kpi.run_anomaly == True) & (Kpi.active == True) & (Kpi.is_static == False)
-        & (Kpi.anomaly_params != None)
     )
-    
+
     task_group = []
 
     for kpi in kpis:
         kpi: Kpi
+
+        # if anomaly isn't setup yet, we still run RCA at     tR + 24 hours
+        # if anomaly is setup, we run both anomaly and RCA at tA + 24 hours
+
         # get scheduler_params, will be None if it's not set
         scheduler_params = (
             kpi.anomaly_params.get("scheduler_params")
@@ -107,39 +110,81 @@ def anomaly_scheduler():
             else None
         )
 
-        # assume 11am if it's not set
-        hour, minute, second = 11, 0, 0
+        scheduled_time = datetime.now()
+        scheduled_time = scheduled_time.replace(hour=11, minute=0, second=0)
         if scheduler_params is not None and "time" in scheduler_params:
             # HH:MM:SS
+            # this is tA
             hour, minute, second = map(int, scheduler_params["time"].split(":"))
 
-        scheduled_time = datetime.now()
-        # today's date, but at 11:00:00am
-        scheduled_time = scheduled_time.replace(hour=hour, minute=minute, second=second)
+            scheduled_time = datetime.now()
+            # today's date, but at HH:MM:SS
+            scheduled_time = scheduled_time.replace(
+                hour=hour, minute=minute, second=second
+            )
+        elif scheduler_params is not None:
+            # today's date, but at rca time (tR) or at kpi creation time
+            scheduled_time = datetime.now()
+            # this is tR
+            if "rca_time" in scheduler_params:
+                hour, minute, second = map(int, scheduler_params["rca_time"].split(":"))
+                scheduled_time = scheduled_time.replace(
+                    hour=hour, minute=minute, second=second
+                )
+            else:
+                # this is kpi creation time
+                canon_time = kpi.created_at
+                scheduled_time = scheduled_time.replace(
+                    hour=canon_time.hour, minute=canon_time.minute, second=canon_time.second
+                )
+
         current_time = datetime.now()
 
-        # check if it's already run
-        already_run = False
-        if (
+        # check if we have to run anomaly:
+        # 1. not already scheduled today
+        # 2. anomaly is set up
+        #
+        # anomaly is setup if model_name is set in anomaly_params
+        anomaly_is_setup = kpi.anomaly_params is not None and "model_name" in kpi.anomaly_params
+        anomaly_already_run = (
             scheduler_params is not None
             and "last_scheduled_time" in scheduler_params
             and datetime.fromisoformat(scheduler_params["last_scheduled_time"])
             > scheduled_time
-        ):
-            already_run = True
+        )
+        to_run_anomaly = (not anomaly_already_run) and anomaly_is_setup
 
-        if not already_run and current_time > scheduled_time:
-            print(f"Scheduling anomaly and RCA for KPI: {kpi.id}")
-            task_group.append(anomaly_single_kpi.s(kpi.id))
-            task_group.append(rca_single_kpi.s(kpi.id))
+        # check if we have to run RCA
+        # 1. if not already scheduled today
+        # 2. if anomaly is scheduled, run RCA too
+        rca_already_run = (
+            scheduler_params is not None
+            and "last_scheduled_time" in scheduler_params
+            and datetime.fromisoformat(scheduler_params["last_scheduled_time"])
+            > scheduled_time
+        )
+        to_run_rca = to_run_anomaly or (not rca_already_run)
+
+        if current_time > scheduled_time and (to_run_rca or to_run_anomaly):
+
+            if to_run_anomaly:
+                print(f"Scheduling anomaly for KPI: {kpi.id}")
+                task_group.append(anomaly_single_kpi.s(kpi.id))
+            if to_run_rca:
+                print(f"Scheduling RCA for KPI: {kpi.id}")
+                task_group.append(rca_single_kpi.s(kpi.id))
 
             new_scheduler_params = (
                 scheduler_params if scheduler_params is not None else {}
             )
-            new_scheduler_params["last_scheduled_time"] = current_time.isoformat()
-            new_scheduler_params["anomaly_status"] = "in-progress"
-            new_scheduler_params["rca_status"] = "in-progress"
-            anomaly_params = kpi.anomaly_params
+            if to_run_anomaly:
+                new_scheduler_params["last_scheduled_time"] = current_time.isoformat()
+                new_scheduler_params["anomaly_status"] = "in-progress"
+            if to_run_rca:
+                new_scheduler_params["last_scheduled_time"] = current_time.isoformat()
+                new_scheduler_params["rca_status"] = "in-progress"
+
+            anomaly_params = kpi.anomaly_params or {}
             anomaly_params["scheduler_params"] = new_scheduler_params
 
             flag_modified(kpi, "anomaly_params")

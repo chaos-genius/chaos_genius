47023aa6c2a3b5dd9da4a3a512853d987dc6d0f3
bhargav <bhargavskumar51703@gmail.com>
2021-10-01 11:34:38 +0530

    feat: added setup and removal scripts

---
+++ docker-cleanup.sh
@@ -0,0 +1,26 @@
+echo "###### REMOVING CONTAINERS AND VOLUMES ###########"
+docker-compose -f rm
+docker volume -f prune
+
+echo "#### CLEANING UP LOCAL DIRECTORIES ###############"
+rm -rf docker/airbyte-db
+rm -rf docker/cg-db
+rm -rf /tmp/workspace
+rm -rf /tmp/airbyte_local
+
+echo "####### REMOVING ALL RELATED IMAGES ##############"
+declare -a image_names=("chaos_genius_celery-scheduler" "chaos_genius_celery-worker" "chaos_genius_chaosgenius-server" "chaos_genius_chaosgenius-webapp" "airbyte/webapp" "airbyte/server" "airbyte/worker" "airbyte/scheduler" "airbyte/db" "airbyte/init" "airbyte/source-google-ads" "airbyte/source-postgres" "airbyte/source-snowflake" "airbyte/source-mysql" "airbyte/source-bing-ads" "airbyte/source-google-sheets" "airbyte/source-shopify" "airbyte/source-stripe" "airbyte/source-bigquery" "airbyte/source-facebook-marketing" "airbyte/source-googleanalytics-singer" "temporalio/auto-setup" "redis" "node" "postgres")
+
+for image in "${image_names[@]}"
+do
+  image_id=$(docker images | grep "$image" | awk '{print $3}')
+  if [ ! -z "$image_id" ]
+  then 
+    echo "Image found. Deleting image........"
+    docker rmi -f $image_id
+  else 
+    echo "image not found"
+  fi
+done
+echo "##### SUCCESSFULLY REMOVED CHAOSGENIUS #########"
+

507ec0f076122f5a2fd0be6a23d9cc525d577770
Manas Solanki <manassolanki@gmail.com>
2021-10-01 17:16:05 +0530

    fix: remove the git merge code line

--- chaos_genius/views/anomaly_data_view.py
+++ chaos_genius/views/anomaly_data_view.py
@@ -595,7 +595,6 @@ def validate_partial_scheduler_params(scheduler_params: Dict[str, Any]) -> Tuple
     return "", scheduler_params
 
 
-<<<<<<< HEAD
 @blueprint.route("/<int:kpi_id>/settings", methods=["GET"])
 # @cache.memoize(timeout=30000)
 def anomaly_settings_status(kpi_id):

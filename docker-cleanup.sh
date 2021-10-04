a3eaeb7a5673605cc124609e9f778e76976dbc70
bhargav <bhargavskumar51703@gmail.com>
2021-10-04 10:07:53 +0530

    fix: updated cleanup script to remove more directories created during runtime

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -7,6 +7,7 @@ rm -rf docker/airbyte-db
 rm -rf docker/cg-db
 rm -rf /tmp/workspace
 rm -rf /tmp/airbyte_local
+rm -rf temporal
 echo "Done"
 echo "####### REMOVING ALL RELATED IMAGES ##############"
 declare -a image_names=("chaos_genius_celery-scheduler" "chaos_genius_celery-worker" "chaos_genius_chaosgenius-server" "chaos_genius_chaosgenius-webapp" "airbyte/webapp" "airbyte/server" "airbyte/worker" "airbyte/scheduler" "airbyte/db" "airbyte/init" "airbyte/source-google-ads" "airbyte/source-postgres" "airbyte/source-snowflake" "airbyte/source-mysql" "airbyte/source-bing-ads" "airbyte/source-google-sheets" "airbyte/source-shopify" "airbyte/source-stripe" "airbyte/source-bigquery" "airbyte/source-facebook-marketing" "airbyte/source-googleanalytics-singer" "temporalio/auto-setup" "redis" "node" "postgres" "chaosgenius/chaosgenius-server" "chaosgenius/chaosgenius-webapp")

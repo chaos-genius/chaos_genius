1ea9cde6292939ecb5df7492d5c6d79d7f614978
Kartikay Bagla <kartikaybagla@gmail.com>
2021-10-06 15:16:30 +0530

    fix(anomaly): circular import in constants

--- chaos_genius/core/anomaly/constants.py
+++ chaos_genius/core/anomaly/constants.py
@@ -1,34 +1,8 @@
 """Provides constants for anomaly detection."""
 
-from chaos_genius.core.anomaly.models.ets_model import ExpTSModel
-from chaos_genius.core.anomaly.models.ewstd_model import EWSTDModel
-from chaos_genius.core.anomaly.models.greykite_model import GreyKiteModel
-from chaos_genius.core.anomaly.models.neuralprophet_model import \
-    NeuralProphetModel
-from chaos_genius.core.anomaly.models.prophet_model import ProphetModel
-from chaos_genius.core.anomaly.models.standard_deviation_model import \
-    StandardDeviationModel
-
 FREQUENCY_DELTA = {"daily": {"days": 1}, "hourly": {"hours": 1}}
 
-MODEL_MAPPER = {
-    "StandardDeviationModel": StandardDeviationModel,
-    "ProphetModel": ProphetModel,
-    "EWSTDModel": EWSTDModel,
-    "NeuralProphetModel": NeuralProphetModel,
-    "GreyKiteModel": GreyKiteModel,
-    "ETSModel": ExpTSModel
-}
-
-FREQUENCY_DELTA = {
-    "daily": {"days": 1},
-    "hourly": {"hours": 1}
-}
-
-RESAMPLE_FREQUENCY = {
-    "daily": "D",
-    "hourly": "H"
-}
+RESAMPLE_FREQUENCY = {"daily": "D", "hourly": "H"}
 
 MODEL_NAME_MAPPING = {
     "StandardDeviationModel": "Standard Deviation",

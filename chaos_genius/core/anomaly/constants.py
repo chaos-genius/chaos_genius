27a31ce5788109d2f0704be06772aebe292c5a95
Kartikay Bagla <kartikaybagla@gmail.com>
2021-10-07 19:29:04 +0530

    chore: commented neuralprophet and greykite
    These models have been commented for this release.

--- chaos_genius/core/anomaly/constants.py
+++ chaos_genius/core/anomaly/constants.py
@@ -13,7 +13,7 @@ MODEL_NAME_MAPPING = {
     "StandardDeviationModel": "Standard Deviation",
     "ProphetModel": "Prophet",
     "EWSTDModel": "Exponentially Weighted Std Dev",
-    "NeuralProphetModel": "NeuralProphet",
-    "GreyKiteModel": "Greykite",
+    # "NeuralProphetModel": "NeuralProphet",
+    # "GreyKiteModel": "Greykite",
     "ETSModel": "ETS"
 }
--- chaos_genius/core/anomaly/models/__init__.py
+++ chaos_genius/core/anomaly/models/__init__.py
@@ -1,11 +1,11 @@
 """Provides models for anomaly detection."""
 
-from chaos_genius.core.anomaly.models.anomaly_model import AnomalyModel
+from chaos_genius.core.anomaly.models.anomaly_model import AnomalyModel  # noqa
 from chaos_genius.core.anomaly.models.ets_model import ExpTSModel
 from chaos_genius.core.anomaly.models.ewstd_model import EWSTDModel
-from chaos_genius.core.anomaly.models.greykite_model import GreyKiteModel
-from chaos_genius.core.anomaly.models.neuralprophet_model import \
-    NeuralProphetModel
+# from chaos_genius.core.anomaly.models.greykite_model import GreyKiteModel
+# from chaos_genius.core.anomaly.models.neuralprophet_model import \
+#     NeuralProphetModel
 from chaos_genius.core.anomaly.models.prophet_model import ProphetModel
 from chaos_genius.core.anomaly.models.standard_deviation_model import \
     StandardDeviationModel
@@ -14,7 +14,7 @@ MODEL_MAPPER = {
     "StandardDeviationModel": StandardDeviationModel,
     "ProphetModel": ProphetModel,
     "EWSTDModel": EWSTDModel,
-    "NeuralProphetModel": NeuralProphetModel,
-    "GreyKiteModel": GreyKiteModel,
+    # "NeuralProphetModel": NeuralProphetModel,
+    # "GreyKiteModel": GreyKiteModel,
     "ETSModel": ExpTSModel,
 }

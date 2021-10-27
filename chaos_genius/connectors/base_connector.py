47202a48c7cf7c4f330eecfff1f41ec0ef41f7f0
Amogh Dhar Diwan <41579921+Fletchersan@users.noreply.github.com>
2021-10-27 18:13:56 +0530

    fix(connectors): fixed typo in base_connector.py

--- chaos_genius/connectors/base_connector.py
+++ chaos_genius/connectors/base_connector.py
@@ -4,6 +4,6 @@ from .connector_utils import merge_dataframe_chunks
 
 def get_df_from_db_uri(db_uri, query):
     engine = create_engine(db_uri)
-    df = merge_dataframe_chunks(pd.read_sql_query(query, engine, chunksize=self.CHUNKSIZE))
+    df = merge_dataframe_chunks(pd.read_sql_query(query, engine, chunksize=20000))
     # pd.read_sql(session.query(Complaint).filter(Complaint.id == 2).statement,session.bind) 
     return df

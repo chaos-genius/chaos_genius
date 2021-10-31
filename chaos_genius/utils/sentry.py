645983a9f872fa037263676f915faa08e8085c45
Manas Solanki <manassolanki@gmail.com>
2021-11-01 01:26:19 +0530

    feat: changes in the logger for initiating the sentry

--- chaos_genius/utils/sentry.py
+++ chaos_genius/utils/sentry.py
@@ -5,33 +5,31 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 
-from chaos_genius.settings import SENTRY_DSN, ENV
 
-
-def init_sentry(flask_integration=False):
+def init_sentry(sentry_dsn, flask_integration=False, env=None):
     """This function will initialised the sentry DSN for the error logging
     and tracking if the env is production
     """
-    if SENTRY_DSN and ENV == 'production':
-        integrations = []
+    print('Initiated the sentry code')
+    integrations = []
 
-        sentry_logging = LoggingIntegration(
-            level=logging.INFO,        # Capture info and above as breadcrumbs
-            event_level=logging.ERROR  # Send errors as events
-        )
-        integrations.append(sentry_logging)
-        integrations.append(RedisIntegration())
-        integrations.append(SqlalchemyIntegration())
+    sentry_logging = LoggingIntegration(
+        level=logging.INFO,        # Capture info and above as breadcrumbs
+        event_level=logging.ERROR  # Send errors as events
+    )
+    integrations.append(sentry_logging)
+    integrations.append(RedisIntegration())
+    integrations.append(SqlalchemyIntegration())
 
-        if flask_integration:
-            integrations.append(FlaskIntegration())
+    if flask_integration:
+        integrations.append(FlaskIntegration())
 
-        sentry_sdk.init(
-            dsn=SENTRY_DSN,
-            integrations=integrations,
-            environment=ENV,
-            before_send=strip_sensitive_data
-        )
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=integrations,
+        environment=env,
+        before_send=strip_sensitive_data
+    )
 
 
 def strip_sensitive_data(event, hint):

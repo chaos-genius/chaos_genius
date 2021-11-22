7e86d8f6e7dc651d7a7d5c03ba9ac4623922a898
Sameer <codetrappers.sameer@gmail.com>
2021-11-22 13:22:36 +0530

    User Profile dropdown

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './navbar.scss';
-// import { Link } from 'react-router-dom';
-// import Profiledown from '../../assets/images/profile-down.svg';
+import { Link } from 'react-router-dom';
+import Setting from '../../assets/images/setting.svg';
+import Profiledown from '../../assets/images/profile-down.svg';
 
 const Navbar = () => {
   return (
@@ -10,31 +11,32 @@ const Navbar = () => {
         <div
           id="userdetails"
           className="userdetails"
-          // data-bs-toggle="dropdown"
-          // aria-expanded="false"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
         >
           <div className="user-avatar">A</div>
           <div className="user-profile">
             <div className="profile-dropdown">
               {/* <p>mathew.thomas@gmail.com</p> */}
               <p>Admin</p>
-              {/* <img src={Profiledown} alt="profile" /> */}
+              <img src={Profiledown} alt="profile" />
             </div>
             {/* <label>Admin</label> */}
           </div>
         </div>
-        {/* <ul className="dropdown-menu" aria-labelledby="userdetails">
-          <li className="active">
-            <Link to="" className="dropdown-item">
-              My Account
+        <ul className="dropdown-menu navbar-dropdown-menu" aria-labelledby="userdetails">
+          <li className="">
+            <Link to="/organization-settings" className="dropdown-item">
+            <img src={Setting} alt="Setting" />
+              <span>Settings</span>
             </Link>
           </li>
-          <li>
+          {/* <li>
             <Link className="dropdown-item" to="">
               Sign Out
             </Link>
-          </li>
-        </ul> */}
+          </li> */}
+        </ul>
       </div>
     </div>
   );

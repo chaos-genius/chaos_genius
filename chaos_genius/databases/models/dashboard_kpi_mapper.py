a22aeae368d24d6060fdd3f6728da24100650edb
Manas Solanki <manassolanki@gmail.com>
2021-11-22 20:34:14 +0530

    feat: add the model for the dashboard

---
+++ chaos_genius/databases/models/dashboard_kpi_mapper.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""DashboardKpiMapper model."""
+import datetime as dt
+from operator import index
+
+from chaos_genius.databases.base_model import Column, PkModel, db
+
+
+class DashboardKpiMapper(PkModel):
+    """A DashboardKpiMapper."""
+
+    __tablename__ = "dashboard_kpi_mapper"
+    dashboard = Column(db.Integer, nullable=False, index=True)
+    kpi = Column(db.Integer, nullable=False, index=True)
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+
+    def __init__(self, name, **kwargs):
+        """Create instance."""
+        super().__init__(name=name, **kwargs)
+
+    def __repr__(self):
+        """Represent instance as a unique string."""
+        return f"<DashboardKpiMapper({self.name})>"
+
+    @property
+    def safe_dict(self):
+        return {
+            "id": self.id,
+        }
+
+    @property
+    def as_dict(self):
+        return {
+            "id": self.id,
+        }

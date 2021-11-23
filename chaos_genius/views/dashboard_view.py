cd39504ec710baf69ca77fb9a87603403d5fcb66
bhargav <bhargavskumar51703@gmail.com>
2021-11-23 11:01:35 +0530

    feat: Add Edit Dashboard API Endpoint
    Added new API Endpoint for editing/updating the dashboard, this handles
    change of dashboard name, adding new kpis to the dashboard and removing kpis from the
    dashboard
    
    Refactored the create dashboard API endpoint to use edit_dashboard_kpis
    controller function internally.
    
    Added another column called 'active' to the DashboardKpiMapper class.

--- chaos_genius/views/dashboard_view.py
+++ chaos_genius/views/dashboard_view.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """Base views for config settings and onboarding."""
 from flask import Blueprint, current_app, request, jsonify
-
+import datetime as dt
 from chaos_genius.controllers.dashboard_controller import (
     create_dashboard,
     get_dashboard_dict_by_id,
     get_dashboard_by_id,
+    edit_dashboard_kpis,
     get_dashboard_list as get_active_dashboard_list,
 )
 
@@ -18,12 +19,12 @@ def create_new_dashboard():
     status, message = "", ""
     try:
         body = request.get_json()
-        name = body["name"]
+        dashboard_name = body["dashboard_name"]
         kpi_list = body["kpi_list"]
-        new_dashboard_dict = create_dashboard(name, kpi_list)
-        new_dashboard = new_dashboard_dict["dashboard"]
-        mapper_list = new_dashboard_dict["mapper_list"]
+        new_dashboard = create_dashboard(dashboard_name)
         new_dashboard.save(commit=True)
+        mapper_list_dict = edit_dashboard_kpis(new_dashboard.id,[],kpi_list)
+        mapper_list = mapper_list_dict["mapper_add_list"]
         for mapper in mapper_list:
             mapper.save(commit=True)
         status = "success"
@@ -37,7 +38,34 @@ def create_new_dashboard():
 
 @blueprint.route("/edit_dashboard", methods=["POST"])
 def edit_dashboard():
-    return jsonify({})
+    status, message = "",""
+    try:
+        body = request.get_json()
+        dashboard_id = body["dashboard_id"]
+        dashboard_name = body["dashboard_name"]
+        kpi_delete_list = body["kpi_delete_list"]
+        kpi_add_list = body["kpi_add_list"]
+
+        dashboard_obj = get_dashboard_by_id(dashboard_id)
+        if dashboard_name != dashboard_obj.name:
+            dashboard_obj.name = dashboard_name
+
+        mapper_list_dict = edit_dashboard_kpis(dashboard_obj.id,kpi_delete_list,kpi_add_list)
+        for mapper_obj in mapper_list_dict["mapper_delete_list"]:
+            mapper_obj.active = False
+            mapper_obj.save(commit=True)
+
+        for mapper_obj in mapper_list_dict["mapper_add_list"]:
+            mapper_obj.save(commit=True)
+
+        dashboard_obj.last_modified = dt.datetime.utcnow
+        dashboard_obj.save(commit=True)
+        status = "success"
+        message = "All changes successfully saved"
+    except Exception as e:
+        status = "failure"
+        message = "Error in editing dashboard: {}".format(e)
+    return jsonify({"status":status, "message":message})
 
 
 @blueprint.route("/get_dashboard", methods=["GET"])

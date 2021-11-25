0233f7a80a04e42f53aa75ffab8fd36ba05d7745
Samyak S Sarnayak <samyak201@gmail.com>
2021-11-25 13:28:17 +0530

    feat(task-table): add Task model, checkpoint helpers
     - Task model has a PK of 2 columns (checkpoint_id, task_id)
        - also added corresponding migration
     - 3 helper functions
        - checkpoint_initial for the first checkpoint, this initializes the
          task_id
        - checkpoint_success and checkpoint_failure are to log success and
          failure of a checkpoint respectively

---
+++ chaos_genius/databases/models/task_model.py
@@ -0,0 +1,45 @@
+"""Model for Tasks (anomaly, deepdrills, etc.)."""
+
+from datetime import datetime
+
+from sqlalchemy.schema import PrimaryKeyConstraint
+
+from chaos_genius.databases.base_model import Column, Model, db
+
+
+class Task(Model):
+    """Stores a checkpoint for a Task."""
+
+    task_id = Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    checkpoint_id = Column(
+        db.Integer, primary_key=True, nullable=False, autoincrement=False, default=1
+    )
+    kpi_id = Column(db.Integer, nullable=False)
+    analytics_type = Column(db.String(80), nullable=False)
+    checkpoint = Column(db.Text(), nullable=False)
+    status = Column(db.String(80), nullable=False)
+    error = Column(db.Text(), nullable=True)
+    timestamp = Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # set PK to (checkpoint_id, task_id)
+    __table_args__ = (
+        PrimaryKeyConstraint('task_id', 'checkpoint_id'),
+    )
+
+    def __repr__(self) -> str:
+        """String representation showing only the task ID."""
+        return f"<Task ({self.task_id})>"
+
+    @property
+    def as_dict(self):
+        """Return a dict representation of the Task checkpoint data."""
+        return {
+            "task_id": self.task_id,
+            "checkpoint_id": self.checkpoint_id,
+            "kpi_id": self.kpi_id,
+            "analytics_type": self.analytics_type,
+            "checkpoint": self.checkpoint,
+            "status": self.status,
+            "timestamp": self.timestamp,
+            "error": self.error,
+        }
---
+++ chaos_genius/jobs/task_monitor.py
@@ -0,0 +1,118 @@
+"""Utilities for logging and monitoring tasks."""
+
+import traceback
+from typing import Optional
+
+from sqlalchemy import func
+
+from chaos_genius.databases.models.task_model import Task
+from chaos_genius.extensions import db
+
+
+def checkpoint_initial(
+    kpi_id: int,
+    analytics_type: str,
+    checkpoint: str,
+    status: str,
+    exc_info: Optional[Exception] = None,
+) -> Task:
+    """Log a task for the first time. Used to get a task_id.
+
+    Args:
+        kpi_id (int): ID for the KPI which this task is associated with.
+        analytics_type (str): type of task being monitored (Anomaly or DeepDrill)
+        checkpoint (str): name or description of this checkpoint.
+        status (str): of this checkpoint. One of "Success" or "Failure".
+        exc_info (Optional[Exception]): exception object, if status is Failure.
+    """
+    error = None
+    if exc_info is not None:
+        error = "".join(traceback.format_tb(exc_info.__traceback__))
+    new_checkpoint = Task(
+        kpi_id=kpi_id,
+        analytics_type=analytics_type,
+        checkpoint=checkpoint,
+        status=status,
+        error=error,
+    )
+    new_checkpoint = new_checkpoint.save(commit=True)
+
+    return new_checkpoint
+
+
+def _checkpoint(
+    task_id: int,
+    kpi_id: int,
+    analytics_type: str,
+    checkpoint: str,
+    status: str,
+    exc_info: Optional[Exception] = None,
+) -> Task:
+    """Log a checkpoint for a task.
+
+    Args:
+        task_id (int): ID for the task to log.
+        kpi_id (int): ID for the KPI which this task is associated with.
+        analytics_type (str): type of task being monitored (Anomaly or DeepDrill)
+        checkpoint (str): name or description of this checkpoint.
+        status (str): of this checkpoint. One of "Success" or "Failure".
+        exc_info (Optional[Exception]): exception object, if status is Failure.
+    """
+    checkpoint_id = (
+        db.session.query(func.max(Task.checkpoint_id))
+        .filter(Task.task_id == task_id)
+        .first()
+        + 1
+    )
+
+    error = None
+    if exc_info is not None:
+        error = "".join(traceback.format_tb(exc_info.__traceback__))
+    new_checkpoint = Task(
+        task_id=task_id,
+        kpi_id=kpi_id,
+        checkpoint_id=checkpoint_id,
+        analytics_type=analytics_type,
+        checkpoint=checkpoint,
+        status=status,
+        error=error,
+    )
+    new_checkpoint = new_checkpoint.save(commit=True)
+
+    return new_checkpoint
+
+
+def checkpoint_success(
+    task_id: int,
+    kpi_id: int,
+    analytics_type: str,
+    checkpoint: str,
+) -> Task:
+    """Log a successful checkpoint for a task.
+
+    Args:
+        task_id (int): ID for the task to log.
+        kpi_id (int): ID for the KPI which this task is associated with.
+        analytics_type (str): type of task being monitored (Anomaly or DeepDrill)
+        checkpoint (str): name or description of this checkpoint.
+    """
+    return _checkpoint(task_id, kpi_id, analytics_type, checkpoint, "Success")
+
+
+def checkpoint_failure(
+    task_id: int,
+    kpi_id: int,
+    analytics_type: str,
+    checkpoint: str,
+    exc_info: Exception
+) -> Task:
+    """Log a failed checkpoint for a task.
+
+    Args:
+        task_id (int): ID for the task to log.
+        kpi_id (int): ID for the KPI which this task is associated with.
+        analytics_type (str): type of task being monitored (Anomaly or DeepDrill)
+        checkpoint (str): name or description of this checkpoint.
+        exc_info (Optional[Exception]): exception object
+    """
+    return _checkpoint(task_id, kpi_id, analytics_type, checkpoint, "Failure", exc_info)

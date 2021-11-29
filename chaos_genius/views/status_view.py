9882e69befa0655fbeb2511ba28a4453fdf71627
Samyak S Sarnayak <samyak201@gmail.com>
2021-11-29 14:03:38 +0530

    fix(status): rename view to status instead of task monitor

--- chaos_genius/views/status_view.py
+++ chaos_genius/views/status_view.py
@@ -1,14 +1,14 @@
-"""Basic view for monitoring analytics tasks."""
+"""A basic view for monitoring services and analytics tasks status."""
 
 from flask import Blueprint, render_template
 
 from chaos_genius.controllers.task_monitor import get_checkpoints
 
-blueprint = Blueprint("monitor", __name__, static_folder="../static")
+blueprint = Blueprint("status", __name__, static_folder="../static")
 
 
 @blueprint.route("/", methods=["GET"])
 def task_monitor_view():
     """A view with a basic UI to monitor analytics tasks."""
     tasks = get_checkpoints(track_subtasks=False)
-    return render_template("jobs.html", tasks=tasks, enumerate=enumerate, str=str)
+    return render_template("status.html", tasks=tasks, enumerate=enumerate, str=str)

01163f2cbb4f1bbd8a133616fa24dad16720d137
Samyak S Sarnayak <samyak201@gmail.com>
2021-12-21 19:35:08 +0530

    test: move flask app creation inside fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,27 +9,26 @@ import pytest
 
 from chaos_genius.app import create_app
 
-app = create_app()
-
 
 @pytest.fixture
 def flask_app():
     """The Flask app to use for app context."""
+    app = create_app()
     return app
 
 
 @pytest.fixture
-def flask_app_context():
+def flask_app_context(flask_app):
     """Fixture that provides flask app context."""
-    with app.app_context() as context:
+    with flask_app.app_context() as context:
         yield context
 
 
 @pytest.fixture
-def flask_client():
+def flask_client(flask_app):
     """Fixture that provides the flask test client.
 
     Can be used to send requests to any configured route.
     """
-    with app.test_client() as client:
+    with flask_app.test_client() as client:
         yield client

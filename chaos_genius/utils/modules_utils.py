ba388d9cdfc3facbe8d06146e6338c2748fc8f00
Manas Solanki <manassolanki@gmail.com>
2021-12-21 16:57:20 +0530

    feat: add the global config api endpoint

---
+++ chaos_genius/utils/modules_utils.py
@@ -0,0 +1,7 @@
+# TODO: Fix this with validation from the CG key
+def is_enterprise_edition():
+    """
+    Checks if the current installation is the enterprise edition.
+    :return: True if the current installation is the enterprise edition.
+    """
+    return False

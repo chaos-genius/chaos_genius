5568a8ee5d27cee380d83c494c901dcefd95bc0e
Kshitij Agarwal <kshitij123456aga@gmail.com>
2021-12-23 08:13:47 +0530

    fix: remove extra schema_name key in as_dict and safe_dict
    - the changes were done in kpi_model.py file

--- chaos_genius/databases/models/kpi_model.py
+++ chaos_genius/databases/models/kpi_model.py
@@ -66,8 +66,7 @@ class Kpi(PkModel):
             "is_static": self.is_static,
             "static_params": self.static_params,
             "active": self.active,
-            "created_at": self.created_at,
-            "schema_name": self.schema_name
+            "created_at": self.created_at
         }
 
     @property
@@ -93,8 +92,7 @@ class Kpi(PkModel):
             "is_static": self.is_static,
             "static_params": self.static_params,
             "active": self.active,
-            "created_at": self.created_at,
-            "schema_name": self.schema_name
+            "created_at": self.created_at
         }
 
     @classmethod

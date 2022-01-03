675312d7c5c0750753db2290dce90c34be0e07c2
bhargav <bhargavskumar51703@gmail.com>
2022-01-03 12:44:47 +0530

    fix: change rquirements file in dockerfile
    Changed requirents file from dev.txt to prod.txt for the backend
    dockerfile.
    
    Changed docker build script to use Dockerfile.prod to build the frontend
    image.

--- scripts/docker/docker-build.sh
+++ scripts/docker/docker-build.sh
@@ -150,7 +150,7 @@ if [ "$ans1" = 'y' ] || [ "$ans1" = '' ];
 then
     echo "Building Frontend Image ......"
     echo
-    docker build -t $FRONTEND_REPO:$FTAG "$BASE_DIR/frontend/"
+    docker build -f "$BASE_DIR/frontend/Dockerfile.prod" -t $FRONTEND_REPO:$FTAG "$BASE_DIR/frontend/"
     echo
 fi
 

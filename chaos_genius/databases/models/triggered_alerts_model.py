d68bd8815289547858e8a4f41a89fdc18bb1bf96
Kshitij Agarwal <kshitij123456aga@gmail.com>
2022-01-17 15:45:32 +0530

    feat: add triggered_alerts_model.py file

---
+++ chaos_genius/databases/models/triggered_alerts_model.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""alert model."""
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.dialects.postgresql import JSONB
+
+from chaos_genius.databases.base_model import Column, PkModel, db
+
+
+class TriggeredAlerts(PkModel):
+    """Storing alerts"""
+    __tablename__ = "triggered_alerts"
+
+    alert_conf_id = Column(db.Integer) #id of the alert configuration using which alert was triggered
+    alert_type = Column(db.String(80), nullable=False) # Event Alert, KPI Alert
+    is_sent = Column(db.Boolean(), default=True, nullable=False, server_default=sqlalchemy.sql.expression.literal(True))
+    alert_metadata = Column(JSONB, default=lambda: {})
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        """Create instance."""
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        """Represent instance as a unique string."""
+        return f"<Triggered Alert ({self.alert_type})>"
+
+    @property
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "alert_conf_id": self.alert_conf_id,
+            "alert_type": self.alert_type,
+            "is_sent": self.is_sent,
+            "alert_metadata": self.alert_metadata,
+            "created_at": self.created_at
+        }

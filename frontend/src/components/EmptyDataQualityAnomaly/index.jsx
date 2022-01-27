85e4fd481165ff0f9166dc4b4e4c676a4ce56233
santhosh kumar <santhosh@sketchbrama.com>
2022-01-27 14:00:18 +0530

    fix:check status button added in fallback screen

--- frontend/src/components/EmptyDataQualityAnomaly/index.jsx
+++ frontend/src/components/EmptyDataQualityAnomaly/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import empty from '../../assets/images/empty-data-quality-anomaly.svg';
 
 const EmptyDataQualityAnomaly = () => {
+  const history = useHistory();
   return (
     <div className="setup-empty-state anomaly-empty-state">
       <div className="empty-state-image">
@@ -10,6 +12,11 @@ const EmptyDataQualityAnomaly = () => {
       </div>
       <h3>No Analysis Found</h3>
       <p>Something went wrong!</p>
+      <button
+        className="btn green-variant-button empty-anomaly-btn"
+        onClick={() => history.push('/task-manager')}>
+        <span>Check status</span>
+      </button>
     </div>
   );
 };

c266ad413a3d146c7d96a9248e59aaefd765f18a
Kshitij Agarwal <kshitij123456aga@gmail.com>
2022-01-31 16:07:28 +0530

    fix: format digest_controller (black and isort)

--- chaos_genius/controllers/digest_controller.py
+++ chaos_genius/controllers/digest_controller.py
@@ -1,15 +1,16 @@
 import datetime
 from typing import List
 
-from chaos_genius.databases.models.triggered_alerts_model import TriggeredAlerts
 from chaos_genius.databases.models.alert_model import Alert
 from chaos_genius.databases.models.kpi_model import Kpi
+from chaos_genius.databases.models.triggered_alerts_model import TriggeredAlerts
+
 
 def triggered_alert_data_processing(data):
     alert_config_cache = dict()
     kpi_cache = dict()
 
-    #TODO optimize to single db call
+    # TODO optimize to single db call
 
     for alert in data:
         id_ = getattr(alert, "alert_conf_id")
@@ -19,7 +20,7 @@ def triggered_alert_data_processing(data):
         else:
             alert_conf = Alert.query.filter(Alert.id == id_).first().as_dict
             alert_config_cache[id_] = alert_conf
-            
+
         kpi_id = alert_conf.get("kpi")
         kpi = None
         if kpi_id is not None:
@@ -28,7 +29,7 @@ def triggered_alert_data_processing(data):
             else:
                 kpi = Kpi.query.filter(Kpi.id == kpi_id).first().as_dict
                 kpi_cache[kpi_id] = kpi
-            
+
         alert.kpi_name = kpi.get("name") if kpi is not None else "Doesn't Exist"
         alert.kpi_id = kpi_id
         alert.alert_name = alert_conf.get("alert_name")
@@ -38,35 +39,44 @@ def triggered_alert_data_processing(data):
         if not isinstance(alert_conf.get("alert_channel_conf"), dict):
             alert.alert_channel_conf = None
         else:
-            alert.alert_channel_conf = alert_conf.get("alert_channel_conf").get(alert.alert_channel, None)
-    
+            alert.alert_channel_conf = alert_conf.get("alert_channel_conf").get(
+                alert.alert_channel, None
+            )
+
     return data
 
 
 def _filter_anomaly_alerts(
-    anomaly_alerts_data: List[TriggeredAlerts],
-    include_subdims: bool = False
+    anomaly_alerts_data: List[TriggeredAlerts], include_subdims: bool = False
 ):
     if not include_subdims:
         for alert in anomaly_alerts_data:
-            alert.alert_metadata["alert_data"] = list(filter(
-                lambda point: point["Dimension"] == "Overall KPI",
-                alert.alert_metadata["alert_data"]
-            ))
+            alert.alert_metadata["alert_data"] = list(
+                filter(
+                    lambda point: point["Dimension"] == "Overall KPI",
+                    alert.alert_metadata["alert_data"],
+                )
+            )
     else:
         for alert in anomaly_alerts_data:
-            overall_anomaly_points = list(filter(
-                lambda point: point["Dimension"] == "Overall KPI",
-                alert.alert_metadata["alert_data"]
-            ))
+            overall_anomaly_points = list(
+                filter(
+                    lambda point: point["Dimension"] == "Overall KPI",
+                    alert.alert_metadata["alert_data"],
+                )
+            )
 
-            subdim_anomaly_points = list(filter(
-                lambda point: point["Dimension"] != "Overall KPI",
-                alert.alert_metadata["alert_data"]
-            ))
+            subdim_anomaly_points = list(
+                filter(
+                    lambda point: point["Dimension"] != "Overall KPI",
+                    alert.alert_metadata["alert_data"],
+                )
+            )
 
             subdim_anomaly_points = subdim_anomaly_points[0:20]
-            alert.alert_metadata["alert_data"] = overall_anomaly_points + subdim_anomaly_points
+            alert.alert_metadata["alert_data"] = (
+                overall_anomaly_points + subdim_anomaly_points
+            )
 
 
 def get_digest_view_data(triggered_alert_id=None, include_subdims: bool = False):
@@ -78,7 +88,11 @@ def get_digest_view_data(triggered_alert_id=None, include_subdims: bool = False)
     if triggered_alert_id is not None:
         filters.append(TriggeredAlerts.id == triggered_alert_id)
 
-    data = TriggeredAlerts.query.filter(*filters).order_by(TriggeredAlerts.created_at.desc()).all()
+    data = (
+        TriggeredAlerts.query.filter(*filters)
+        .order_by(TriggeredAlerts.created_at.desc())
+        .all()
+    )
     data = triggered_alert_data_processing(data)
 
     anomaly_alerts_data = [alert for alert in data if alert.alert_type == "KPI Alert"]
@@ -86,7 +100,3 @@ def get_digest_view_data(triggered_alert_id=None, include_subdims: bool = False)
     event_alerts_data = [alert for alert in data if alert.alert_type == "Event Alert"]
 
     return anomaly_alerts_data, event_alerts_data
-
-
-
-            

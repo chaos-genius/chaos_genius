5c00f55fa70e990a02313c0e3114aa21482ab9ac
Debanjan Dey <chartisdev@Debanjans-MacBook-Pro.local>
2022-02-01 18:42:36 +0530

    tab navigation of hpi dashboards should not reload the page

--- frontend/src/components/Homefilter/index.jsx
+++ frontend/src/components/Homefilter/index.jsx
@@ -9,7 +9,7 @@ import { formatDateTime } from '../../utils/date-helper';
 import { useHistory } from 'react-router-dom';
 import { CustomTooltip } from '../../utils/tooltip-helper';
 
-const Homefilter = ({ data, setDashboard, dashboard }) => {
+const Homefilter = ({ data, setDashboard, dashboard, setDashboardId }) => {
   const [filterData, setFilterData] = useState(data);
 
   const history = useHistory();
@@ -93,12 +93,13 @@ const Homefilter = ({ data, setDashboard, dashboard }) => {
             filterData.map((item) => {
               return (
                 <li
+                  key={item.id}
                   className={
                     dashboard.toString() === item.id.toString() ? 'active' : ''
                   }
                   onClick={() => {
                     setDashboard(item.id);
-                    history.push(`${item?.id}`);
+                    setDashboardId(item.id);
                   }}>
                   <div className="filter-tooltipcontent">
                     {CustomTooltip(item.name)}

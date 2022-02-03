9589be35dfdb9d07fea1223211f597e876794fdc
Kartikay Bagla <kartikaybagla@gmail.com>
2022-02-03 12:07:37 +0530

    chore: update util api functions

--- chaos_genius/utils/datetime_helper.py
+++ chaos_genius/utils/datetime_helper.py
@@ -1,13 +1,15 @@
-from datetime import datetime, timezone
+from datetime import date, datetime, timezone
 
 
 def get_server_timezone():
     return datetime.now(timezone.utc).astimezone().tzname()
 
 
-def get_rca_timestamp(date):
-    return datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
+def get_rca_timestamp(date_value):
+    return datetime.strptime(date_value, '%Y/%m/%d %H:%M:%S').date()
 
 
-def get_epoch_timestamp(date):
-    return int(datetime.timestamp(date)) * 1000
+def get_epoch_timestamp(date_value):
+    if isinstance(date_value, date):
+        date_value = datetime(date_value.year, date_value.month, date_value.day)
+    return int(datetime.timestamp(date_value)) * 1000

7a8f7fccb177830228d6a7ea15b95b3d4f0f30dd
santhosh kumar <santhosh@sketchbrama.com>
2022-02-04 19:37:15 +0530

    fix:ui changes

--- frontend/src/components/KpiFaq/index.jsx
+++ frontend/src/components/KpiFaq/index.jsx
@@ -9,85 +9,100 @@ const AddFaq = () => {
         <div className="faq__title">
           <h2>FAQs</h2>
         </div>
-        <div className="faq__separate_links">
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#add-a-kpi"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>Add a KPI</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#table-vs-custom-query-kpis"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>Table vs Custom Query KPIs</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-parameters"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>KPI Parameters</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-validation"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>KPI Validation</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#add-a-kpi"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>Add a KPI</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#table-vs-custom-query-kpis"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>Table vs Custom Query KPIs</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-parameters"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>KPI Parameters</label>
-          </a>
-          <a
-            href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-validation"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>KPI Validation</label>
-          </a>
-        </div>
+        <ul className="faq__separate_links">
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#add-a-kpi"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>Add a KPI</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#table-vs-custom-query-kpis"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>Table vs Custom Query KPIs</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-parameters"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>KPI Parameters</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-validation"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>KPI Validation</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#add-a-kpi"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>Add a KPI</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#table-vs-custom-query-kpis"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>Table vs Custom Query KPIs</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-parameters"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>KPI Parameters</label>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.chaosgenius.io/docs/kpi_explorer/adding-kpis#kpi-validation"
+              target="_blank"
+              rel="noopener noreferrer">
+              {' '}
+              <label>KPI Validation</label>
+            </a>
+          </li>
+        </ul>
       </div>
       <div className="faq__wrapper-content">
         <div className="faq__title">
           <h2>Video tutorials</h2>
         </div>
-        <div className="faq__separate_links">
-          <a
-            href="https://www.youtube.com/embed/Z-kbVFh1zB8"
-            target="_blank"
-            rel="noopener noreferrer">
-            {' '}
-            <label>Chaos genius 101</label>
-          </a>
-        </div>
+        <ul className="faq__separate_links">
+          <li>
+            <a
+              href="https://www.youtube.com/embed/Z-kbVFh1zB8"
+              target="_blank"
+              rel="noopener noreferrer">
+              <label>Chaos genius 101</label>
+            </a>
+          </li>
+        </ul>
       </div>
       <div className="faq__support-content">
         <p>
           Need help? Feel free to send us a message at{' '}
-          <a href="mailto: support@Chaosgenius.com" rel="noopener noreferrer">
-            support@Chaosgenius.com
-          </a>{' '}
+          <a href="mailto: support@Chaosgenius.com">support@Chaosgenius.com</a>{' '}
         </p>
       </div>
     </div>

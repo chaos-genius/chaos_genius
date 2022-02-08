103a5cda45e8a268cf0c680166eeadf1d81e069c
Samyak S Sarnayak <samyak201@gmail.com>
2022-02-08 16:44:57 +0530

    chore: format digest_view.py

--- chaos_genius/views/digest_view.py
+++ chaos_genius/views/digest_view.py
@@ -1,17 +1,18 @@
-"""View to handle requests pertaining to alert digests"""
+"""View to handle requests pertaining to alert digests."""
 
 import logging
-from typing import Dict, Optional, cast
-from flask import Blueprint, render_template, jsonify, request
+
+from flask import Blueprint, render_template, request
+
 from chaos_genius.controllers.digest_controller import get_digest_view_data
 
 blueprint = Blueprint("digest", __name__, static_folder="../static")
 logger = logging.getLogger(__name__)
 
+
 @blueprint.route("", methods=["GET"])
 def task_monitor_view():
-    """A view that sends triggered alerts data"""
-
+    """A view that displays triggered alerts data."""
     triggered_alert_id = request.args.get("id", None)
     include_subdims = request.args.get("subdims", False)
     if include_subdims is not False:

99eaf07d68f2bb7a02d1dc8efcc39e2a660e9110
Kartikay Bagla <kartikaybagla@gmail.com>
2022-02-10 11:48:28 +0530

    feat: added limited timezone support

--- chaos_genius/utils/datetime_helper.py
+++ chaos_genius/utils/datetime_helper.py
@@ -1,17 +1,21 @@
-from datetime import date, datetime, timezone
+"""Provides helper functions related to datetime operations."""
+
+from datetime import datetime, timezone
+
+from chaos_genius.core.utils.constants import SUPPORTED_TIMEZONES
+from chaos_genius.settings import TIMEZONE
 
 
 def get_server_timezone():
+    """Get server timezone."""
     return datetime.now(timezone.utc).astimezone().tzname()
 
 
-def get_rca_timestamp(date_value):
+def get_rca_date_from_string(date_value):
+    """Get RCA date from string."""
     return datetime.strptime(date_value, "%Y/%m/%d %H:%M:%S").date()
 
 
-def get_epoch_timestamp(date_value):
-    if isinstance(date_value, date):
-        date_value = datetime(
-            date_value.year, date_value.month, date_value.day
-        )
-    return int(datetime.timestamp(date_value)) * 1000
+def get_date_string_with_tz(date_value) -> str:
+    """Get date string with timezone."""
+    return date_value.strftime("%d %b %Y") + f" {TIMEZONE} ({SUPPORTED_TIMEZONES[TIMEZONE]})"

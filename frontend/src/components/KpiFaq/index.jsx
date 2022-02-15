acead5073d364c64d2c1bd94159eeefe5837f7c2
Debanjan Dey <chartisdev@Debanjans-MacBook-Pro.local>
2022-02-15 12:27:24 +0530

    spelling error fixed

--- frontend/src/components/KpiFaq/index.jsx
+++ frontend/src/components/KpiFaq/index.jsx
@@ -24,7 +24,7 @@ const AddFaq = () => {
   };
   const faqList = [
     {
-      subheading: 'large Data Sets',
+      subheading: 'Large Data Sets',
       content: [
         {
           href: '',

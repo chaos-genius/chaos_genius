bc2278b463acde49bbc385db7fbcf001664b23be
Samyak S Sarnayak <samyak201@gmail.com>
2022-03-02 15:47:51 +0530

    fix(scheduler): typo on anomaly scheduler beat schedule

--- chaos_genius/celery_config.py
+++ chaos_genius/celery_config.py
@@ -13,7 +13,7 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERYBEAT_SCHEDULE = {
     "anomaly-scheduler": {
         "task": "chaos_genius.jobs.analytics_scheduler.scheduler_wrapper",
-        "schedule": schedule(timedelta(minute=1)),
+        "schedule": schedule(timedelta(minutes=1)),
         "args": ()
     },
     'alerts-daily': {

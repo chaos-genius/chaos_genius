e8bc9163da0fae5378f63d95be32415a3d7506b0
Kartikay Bagla <kartikaybagla@gmail.com>
2022-03-07 08:11:47 +0100

    chore: updated test for string utils

--- tests/test_rca_utils_strings.py
+++ tests/test_rca_utils_strings.py
@@ -5,9 +5,36 @@ import pandas as pd
 from chaos_genius.core.utils.string_helpers import (
     convert_df_dims_to_query_strings,
     convert_query_string_to_user_string,
+    get_query_from_col_and_value,
+    get_query_from_cols_and_values
 )
 
 # TODO: Add tests for binned values as well
+
+def test_get_query_from_col_and_value():
+    """Test function to get query string."""
+    values = [
+        (("a", "b"), "`a`==\"b\""),
+        (("\"a\"", "\"b\""), "`\\\"a\\\"`==\"\\\"b\\\"\"")
+    ]
+
+    for (col, val), output in values:
+        assert get_query_from_col_and_value(col, val) == output
+
+
+def test_get_query_from_cols_and_values():
+    """Test function to get query strings."""
+    dimensions = ["a", "\"a\""]
+    subgroups = [
+        "b",
+        ("c", "\"c\"")
+    ]
+    output = [
+        "`a`==\"b\"",
+        "`a`==\"c\" and `\\\"a\\\"`==\"\\\"c\\\"\""
+    ]
+
+    assert get_query_from_cols_and_values(dimensions, subgroups) == output
 
 
 def test_convert_df_dims_to_query_string():

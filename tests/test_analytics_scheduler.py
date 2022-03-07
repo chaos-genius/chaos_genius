e64074ffe9f64a4612551361a5de019d4299d081
Rajdeep Sharma <rjdp9736@gmail.com>
2022-03-07 09:57:16 +0530

    test: add more tests for _to_run_rca, _to_run_anomaly, _get_scheduled_time_daily, _get_scheduled_time_hourly

--- tests/test_analytics_scheduler.py
+++ tests/test_analytics_scheduler.py
@@ -1,80 +1,11 @@
-"""Tests for analytics_scheduler"""
+from datetime import datetime, timedelta
 
-# Test cases:
-# 1 minute/1 hour since last scheduled - should not schedule again  run  (last_scheduled_time_rca, last_scheduled_time_anomaly attr for rca, anomaly resp) ## both run_rca , run_anomanly
-# 23 hours since last scheduled - should not schedule again
-# 24 hours + 1 minute since last scheduled - should schedule again
-
-# Anomaly not setup
-# 1 hour since KPI setup - should not schedule again
-# 24 hours + 1 minute since KPI setup - should schedule
-# Anomaly should not run in any case, only DeepDrills
-# Anomaly setup after DeepDrills has run
-# DeepDrills should not run at 24 hours after first run
-# DeepDrills should run 24 hours after anomaly run
-
-
-# rca is always daily and anomaly can be both hourly and daily
-# for rca setup time is scheduled time and next time it will run at same time and can be overriden by using rca_time in scheduler params field
-# for anomaly  scheduled time  is time field in scheduler params scheduler_params={ "time": "11:00:00",
-
-# if model_name in anomaly_params is not set we assume anomaly is not setup
-
-# anomaly runs when configured and then on the scheduled time going forward
-
-# for hourly unit test pass scheduler_frequency  as "H" in scheduler params
-
-
-# hypothesis
-
-# https://docs.celeryproject.org/en/stable/userguide/testing.html
-
-# Integration Tests
-# Test anomaly/deepdrills invocation outside of scheduler:
-#     When KPI is setup:
-# Deepdrils should run
-# When anomaly is setup:
-#     Only anomaly should run
-#     Next DeepDrills should run with anomaly
-
-
-# True and False cases for to_run_anomaly
-#   Various scheduled time to confirm behavior
-#      - Play with now (datetime.now and last_scheduled_time_anomaly)
-#      - Curr_time < scheduled_time ⇒  dont run  {prepare input values}
-#      - Curr > scheduled time , play on last_scheduled_time_anomaly => (run or not based last_scheduled_time_anomaly value )
-
-# We can use the above cases of scheduler frequency to test for this (H, D)
-
-# True and False cases for _to_run_rca
-#   - Various scheduled time to confirm behavior
-#       - Play with (datetime.now and last_scheduled_time_rca)
-#   - We can use the above cases of scheduler frequency to test for this (D)
-
-
-# _get_scheduled_time_hourly
-# 9:11pm =>9:10 ,9:59=> 9:10, 00:12 => 00:10  (kinda dont makes sense but ok like test now()) , scheduler_params.time = 11:12 => now_date now_hr:11:12
-# _to_run_anomaly
-# _to_run_rca
-
-
-# _get_scheduled_time_daily
-
-# (kpi.scheduler_params.time overrides kpi.created_at)
-# if kpi.scheduler_params.time ==> now_date input_hr:input_min:input sec
-# else ==> now_date created_at_hr: created_at_min:created_at
-
-# if case => hh:mm:ss => now_date hh:mm:ss # ss is 00 implicit
-# else dd/mm/yy hh:mm:ss => now_Date hh:mm:ss
-
-
-from datetime import datetime
-
-from sqlalchemy.sql.expression import delete
 from chaos_genius.databases.models.kpi_model import Kpi
 from chaos_genius.jobs.analytics_scheduler import AnalyticsScheduler
 
 from copy import deepcopy
+
+import pytest
 
 test_kpi = Kpi(
     name="AOV",
@@ -118,202 +49,105 @@ def test_get_scheduled_time_11():
     )
 
 
-# Scheduler Freq = Daily
-# =========================
+now = datetime.now()
 
-# RCA setup(its always setup .. duh!) , anomaly not setup (model_name is not present ..) => kpi_type_A
-# Scheduled time(scheduled_time param to run_{rca,anomaly})   ==  kpi creation time (kpi.scheduler_params.rca_time or kpi.created_at)
+make_2_digit = lambda x: f"0{x}" if x < 10 else x
+now_hr = make_2_digit(now.hour)
+now_min = make_2_digit(now.minute)
+now_sec = make_2_digit(now.second)
+now_hr = make_2_digit(now.hour)
+now_date = str(now.date())
 
-test_kpi_A = Kpi(
-    name="AOV",
-    is_certified="false",
-    data_source=1,
-    kpi_type="table",
-    kpi_query="",
-    schema_name="public",
-    table_name="ecom_retail_data",
-    metric="ItemTotalPrice",
-    aggregation="mean",
-    datetime_column="date",
-    filters=[],
-    dimensions=["DayOfWeek", "PurchaseTime", "Country"],
-    anomaly_params={
-        "anomaly_period": 60,
-        "seasonality": [],
-        "frequency": "D",
-        # "model_name": "ProphetModel",
-        "sensitivity": "medium",
-    },
-    scheduler_params={
-        "time": "11:00:00",
-        "rca_status": "completed",
-        # "rca_time"
-        "anomaly_status": "completed",
-        "scheduler_frequency": "D",
-        "last_scheduled_time_rca": "2022-02-24T11:51:26.358842",
-        "last_scheduled_time_anomaly": "2022-02-24T11:51:26.346090",
-    },
-)
-
-# RCA setup and anomaly setup (model_name is present)
-# Scheduler time (scheduled_time param to run_{rca,anomaly}) == Anomaly setup time (kpi.schedular_params.time)
-
-
-test_kpi_B = Kpi(
-    name="AOV",
-    is_certified="false",
-    data_source=1,
-    kpi_type="table",
-    kpi_query="",
-    schema_name="public",
-    table_name="ecom_retail_data",
-    metric="ItemTotalPrice",
-    aggregation="mean",
-    datetime_column="date",
-    filters=[],
-    dimensions=["DayOfWeek", "PurchaseTime", "Country"],
-    anomaly_params={
-        "anomaly_period": 60,
-        "seasonality": [],
-        "frequency": "D",
-        "model_name": "ProphetModel",
-        "sensitivity": "medium",
-    },
-    scheduler_params={
-        "time": "11:00:00",
-        "rca_status": "completed",
-        # "rca_time"
-        "anomaly_status": "completed",
-        "scheduler_frequency": "D",
-        "last_scheduled_time_rca": "2022-02-24T11:51:26.358842",
-        "last_scheduled_time_anomaly": "2022-02-24T11:51:26.346090",
-    },
-)
-
-
-# Scheduler Freq = Hourly
-# =========================
-
-# RCA setup, anomaly not setup  => kpi_type_C
-
-test_kpi_C = Kpi(
-    name="AOV",
-    is_certified="false",
-    data_source=1,
-    kpi_type="table",
-    kpi_query="",
-    schema_name="public",
-    table_name="ecom_retail_data",
-    metric="ItemTotalPrice",
-    aggregation="mean",
-    datetime_column="date",
-    filters=[],
-    dimensions=["DayOfWeek", "PurchaseTime", "Country"],
-    anomaly_params={
-        "anomaly_period": 60,
-        "seasonality": [],
-        "frequency": "H",
-        # "model_name": "ProphetModel",
-        "sensitivity": "medium",
-    },
-    scheduler_params={
-        "time": "11:00:00",
-        "rca_status": "completed",
-        # "rca_time"
-        "anomaly_status": "completed",
-        "scheduler_frequency": "D",
-        "last_scheduled_time_rca": "2022-02-24T11:51:26.358842",
-        "last_scheduled_time_anomaly": "2022-02-24T11:51:26.346090",
-    },
-)
-
-# RCA setup, anomaly setup
-
-
-test_kpi_D = Kpi(
-    name="AOV",
-    is_certified="false",
-    data_source=1,
-    kpi_type="table",
-    kpi_query="",
-    schema_name="public",
-    table_name="ecom_retail_data",
-    metric="ItemTotalPrice",
-    aggregation="mean",
-    datetime_column="date",
-    filters=[],
-    dimensions=["DayOfWeek", "PurchaseTime", "Country"],
-    anomaly_params={
-        "anomaly_period": 60,
-        "seasonality": [],
-        "frequency": "H",
-        # "model_name": "ProphetModel",
-        "sensitivity": "medium",
-    },
-    scheduler_params={
-        "time": "11:00:00",
-        "rca_status": "completed",
-        # "rca_time"
-        "anomaly_status": "completed",
-        "scheduler_frequency": "D",
-        "last_scheduled_time_rca": "2022-02-24T11:51:26.358842",
-        "last_scheduled_time_anomaly": "2022-02-24T11:51:26.346090",
-    },
-)
-
-
-now_hr = datetime.now().hour
-now_date = str(datetime.now().date())
-test_cases_get_scheduled_time_hourly_positive = [
+test_cases_get_scheduled_time_hourly = [
     ("10:12", f"{now_date} {now_hr}:10:12"),
     ("00:59", f"{now_date} {now_hr}:00:59"),
     ("23:59", f"{now_date} {now_hr}:23:59"),
     ("00:59", f"{now_date} {now_hr}:00:59"),
-    ("00:59", f"{now_date} {now_hr}:00:59"),
+    ("07:21", f"{now_date} {now_hr}:07:21"),
+    ("00:00", f"{now_date} {now_hr}:00:00"),
     ("", f"{now_date} {now_hr}:10:00"),
     ("11:15:56", f"{now_date} {now_hr}:10:00"),
+    ("23:15:56", f"{now_date} {now_hr}:10:00"),
+    ("00:11:56", f"{now_date} {now_hr}:10:00"),
+    ("08:45:56", f"{now_date} {now_hr}:10:00"),
 ]
 
-FOUR_TYPES_OF_KPI_OBJS = [test_kpi_A, test_kpi_B, test_kpi_C, test_kpi_D]
 
-# test _get_scheduled_time_hourly
-# depends on kpi.scheduler_params.time (only 1 param) and implicit datetime.now()
-# expected inputs formats /output
-#    mm:ss ==> now_date now_hr:mm:ss
-#    hh:mm:ss ==> now_date now_hr:10:00
-
-
-import pytest
-
-
-@pytest.mark.parametrize("time,expected", test_cases_get_scheduled_time_hourly_positive)
-def test_get_scheduled_time_hourly_positive(time, expected):
-    # kpis_deep_copy = deepcopy(FOUR_TYPES_OF_KPI_OBJS)
-    # for kpi in kpis_deep_copy:
-    #     if time:
-    #         kpi.scheduler_params["time"] = time
-    #     else:
-    #         del kpi.scheduler_params["time"]
+@pytest.mark.parametrize("time,expected", test_cases_get_scheduled_time_hourly)
+def test_get_scheduled_time_hourly(time, expected):
     kpi = deepcopy(test_kpi)
-    kpi.scheduler_params["time"] = time
+    if time:
+        kpi.scheduler_params["time"] = time
+    else:
+        del kpi.scheduler_params["time"]
     scheduler = AnalyticsScheduler()
-
-    # print(time, " # ", expected, " # ", scheduler._get_scheduled_time_hourly(kpi).strftime('%Y-%m-%d %H:%M:%S'), " # ", datetime.strptime(expected, '%Y-%m-%d %H:%M:%S'))
-    assert (
-        scheduler._get_scheduled_time_hourly(kpi).strftime("%Y-%m-%d %H:%M:%S")
-        == expected
-    )
+    assert scheduler._get_scheduled_time_hourly(kpi).strftime(DT_FMT) == expected
 
 
-# test _to_run_anomaly
-# depends on scheduled_time
+DT_FMT = "%Y-%m-%d %H:%M:%S"
 
-# if not setup model_name absent => returns false
+# TODO : rca_time
+test_cases_get_scheduled_time_daily = [
+    ("", now, f"{now_date} {now_hr}:{now_min}:{now_sec}"),
+    ("", now - timedelta(days=1), f"{now_date} {now_hr}:{now_min}:{now_sec}"),
+    ("", now - timedelta(days=800), f"{now_date} {now_hr}:{now_min}:{now_sec}"),
+    (
+        "",
+        now - timedelta(hours=2),
+        f"{now_date} {make_2_digit((now - timedelta(hours=2)).hour)}:{now_min}:{now_sec}",
+    ),
+    (
+        "",
+        now - timedelta(minutes=35, days=4),
+        f"{now_date} {make_2_digit((now - timedelta(minutes=35)).hour)}:{make_2_digit((now - timedelta(minutes=35)).minute)}:{now_sec}",
+    ),  # time not empty
+    (
+        "00:01:02",
+        now - timedelta(seconds=35, days=3),
+        f"{now_date} 00:01:02",
+    ),
+    ("05:10:19", now, f"{now_date} 05:10:19"),
+    ("23:59:59", now - timedelta(days=1), f"{now_date} 23:59:59"),
+    ("12:12:01", now - timedelta(days=800), f"{now_date} 12:12:01"),
+    (
+        "00:00:01",
+        now - timedelta(hours=2),
+        f"{now_date} 00:00:01",
+    ),
+    (
+        "17:59:01",
+        now - timedelta(minutes=35, days=4),
+        f"{now_date} 17:59:01",
+    ),
+    (
+        "21:08:10",
+        now - timedelta(seconds=35, days=3),
+        f"{now_date} 21:08:10",
+    ),
+]
 
 
-test_cases_test_to_run_anomaly_positive = [
-    ({"anomaly_params": {}}, f"{now_date} {now_hr}:12:19", False),
-    ({"scheduler_params": {}}, f"{now_date} {now_hr}:12:19", True),
+@pytest.mark.parametrize(
+    "time,created_at,expected", test_cases_get_scheduled_time_daily
+)
+def test_get_scheduled_time_daily(time, created_at, expected):
+    kpi = deepcopy(test_kpi)
+    kpi.created_at = created_at
+    if time:
+        kpi.scheduler_params["time"] = time
+    else:
+        del kpi.scheduler_params["time"]
+    scheduler = AnalyticsScheduler()
+    assert scheduler._get_scheduled_time_daily(kpi).strftime(DT_FMT) == expected
+
+
+test_cases_test_to_run_anomaly = [
+    ({"anomaly_params": {}}, f"{now_date} {now_hr}:12:19", False),  # anomaly not setup
+    (
+        {"scheduler_params": {}},
+        f"{now_date} {now_hr}:12:19",
+        True,
+    ),  # need to recheck this
     (
         {
             "scheduler_params": {
@@ -332,27 +166,149 @@ test_cases_test_to_run_anomaly_positive = [
         f"{now_date} {now_hr}:12:10",
         True,
     ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_anomaly": f"{now_date} {now_hr}:15:12"
+            }
+        },
+        f"{now_date} {now_hr}:12:14",
+        False,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_anomaly": f"{now_date} {now_hr}:49:08"
+            }
+        },
+        f"{now_date} {now_hr}:10:00",
+        False,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_anomaly": f"{(now - timedelta(days=800)).date()} {now_hr}:15:12"
+            }
+        },
+        f"{now_date} {now_hr}:12:14",
+        True,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_anomaly": f"{(now - timedelta(hours=2)).date()} {make_2_digit((now - timedelta(hours=2)).hour)}:49:08"
+            }
+        },
+        f"{now_date} {now_hr}:10:00",
+        True,
+    ),
 ]
 
 
 @pytest.mark.parametrize(
-    "kpi_params,scheduled_time,expected", test_cases_test_to_run_anomaly_positive
+    "kpi_params,scheduled_time,expected", test_cases_test_to_run_anomaly
 )
 def test_to_run_anomaly(kpi_params, scheduled_time, expected):
-    # kpis_deep_copy = deepcopy(FOUR_TYPES_OF_KPI_OBJS)
-    # for kpi in kpis_deep_copy:
-    #     if time:
-    #         kpi.scheduler_params["time"] = time
-    #     else:
-    #         del kpi.scheduler_params["time"]
     kpi = deepcopy(test_kpi)
     for key in kpi_params:
         setattr(kpi, key, kpi_params[key])
     scheduler = AnalyticsScheduler()
-    scheduled_time = datetime.strptime(scheduled_time, "%Y-%m-%d %H:%M:%S")
+    scheduled_time = datetime.strptime(scheduled_time, DT_FMT)
 
-    # print(time, " # ", expected, " # ", scheduler._get_scheduled_time_hourly(kpi).strftime('%Y-%m-%d %H:%M:%S'), " # ", datetime.strptime(expected, '%Y-%m-%d %H:%M:%S'))
     assert scheduler._to_run_anomaly(kpi, scheduled_time) == expected
 
 
-# _to_run_anomaly
+test_cases_test_to_run_rca = [
+    (
+        {"anomaly_params": {}, "scheduler_params": {}},
+        f"{now_date} {now_hr}:12:19",
+        True,
+    ),
+    ({"scheduler_params": {}}, f"{now_date} {now_hr}:12:19", True),
+    (
+        {"scheduler_params": {"last_scheduled_time_rca": f"{now_date} {now_hr}:12:12"}},
+        f"{now_date} {now_hr}:12:14",
+        True,
+    ),
+    (
+        {"scheduler_params": {"last_scheduled_time_rca": f"{now_date} {now_hr}:12:08"}},
+        f"{now_date} {now_hr}:12:10",
+        True,
+    ),
+    (
+        {"scheduler_params": {"last_scheduled_time_rca": f"{now_date} {now_hr}:15:12"}},
+        f"{now_date} {now_hr}:12:14",
+        False,
+    ),
+    (
+        {"scheduler_params": {"last_scheduled_time_rca": f"{now_date} {now_hr}:49:08"}},
+        f"{now_date} {now_hr}:10:00",
+        False,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(days=800)).date()} {now_hr}:15:12"
+            }
+        },
+        f"{now_date} {now_hr}:12:14",
+        True,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(hours=2)).date()} {make_2_digit((now - timedelta(hours=2)).hour)}:49:08"
+            }
+        },
+        f"{now_date} {now_hr}:10:00",
+        True,
+    ),  # -1 day
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(days=1)).date()} {now_hr}:17:12"
+            }
+        },
+        f"{now_date} {now_hr}:12:14",
+        True,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(days=1)).date()} {now_hr}:06:08"
+            }
+        },
+        f"{now_date} {now_hr}:12:10",
+        True,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(days=1)).date()} {now_hr}:15:12"
+            }
+        },
+        f"{now_date} {now_hr}:12:14",
+        True,
+    ),
+    (
+        {
+            "scheduler_params": {
+                "last_scheduled_time_rca": f"{(now - timedelta(days=1)).date()} {now_hr}:49:08"
+            }
+        },
+        f"{now_date} {now_hr}:10:00",
+        True,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "kpi_params,scheduled_time,expected", test_cases_test_to_run_rca
+)
+def test_to_run_rca(kpi_params, scheduled_time, expected):
+    kpi = deepcopy(test_kpi)
+    for key in kpi_params:
+        setattr(kpi, key, kpi_params[key])
+    scheduler = AnalyticsScheduler()
+    scheduled_time = datetime.strptime(scheduled_time, DT_FMT)
+    assert scheduler._to_run_rca(kpi, scheduled_time) == expected

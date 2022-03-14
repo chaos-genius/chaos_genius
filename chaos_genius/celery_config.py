93bc6d8dc3ee2887df6f0c8e39851c2c90cab833
Manas Solanki <manassolanki@gmail.com>
2022-03-14 11:06:36 +0530

    feat: add the hourly schedular for the event alerts

--- chaos_genius/celery_config.py
+++ chaos_genius/celery_config.py
@@ -14,48 +14,26 @@ CELERYBEAT_SCHEDULE = {
     "anomaly-scheduler": {
         "task": "chaos_genius.jobs.analytics_scheduler.scheduler_wrapper",
         "schedule": schedule(timedelta(minutes=1)),
-        "args": ()
-    },
-    'alerts-daily': {
-        'task': 'chaos_genius.jobs.alert_tasks.check_event_alerts',
-        'schedule': crontab(hour="3", minute="0"),  # Daily: at 3am
-        'args': ('daily',)
+        "args": (),
     },
     "alert-digest-daily-scheduler": {
         "task": "chaos_genius.jobs.alert_tasks.alert_digest_daily_scheduler",
         "schedule": schedule(timedelta(minutes=10)),
-        "args": ()
+        "args": (),
     },
-    # 'anomaly-task-every-minute': {
-    #     'task': 'chaos_genius.jobs.anomaly_tasks.add_together',
-    #     'schedule': crontab(minute="*"), # Every minutes
-    #     'args': (5,10,)
-    # },
-    # "anomaly-tasks-all-kpis": {
-    #     "task": "chaos_genius.jobs.anomaly_tasks.anomaly_kpi",
-    #     # "schedule": crontab(hour=[11]),
-    #     "schedule": schedule(timedelta(minutes=1)),  # for testing
-    #     "args": ()
-    # },
-    # 'alerts-weekly': {
-    #     'task': 'chaos_genius.jobs.alert_tasks.check_event_alerts',
-    #     'schedule': crontab(day_of_week="0"), # Weekly: every sunday
-    #     'args': ('weekly',)
-    # },
-    # 'alerts-hourly': {
-    #     'task': 'chaos_genius.jobs.alert_tasks.check_event_alerts',
-    #     'schedule': crontab(hour="*"), # Hourly: at 0th minute
-    #     'args': ('hourly',)
-    # },
-    # 'alerts-every-15-minute': {
-    #     'task': 'chaos_genius.jobs.alert_tasks.check_event_alerts',
-    #     'schedule': crontab(minute="*/15"), # Every 15 minutes
-    #     'args': ('every_15_minute',)
-    # }
+    "alerts-daily": {
+        "task": "chaos_genius.jobs.alert_tasks.check_event_alerts",
+        "schedule": crontab(hour="3", minute="0"),  # Daily: at 3am
+        "args": ("daily",),
+    },
+    "alerts-hourly": {
+        "task": "chaos_genius.jobs.alert_tasks.check_event_alerts",
+        "schedule": crontab(minute="0"),  # Hourly: at 0th minute
+        "args": ("hourly",),
+    },
 }
 
 CELERY_ROUTES = {
-    
     "chaos_genius.jobs.anomaly_tasks.*": {"queue": "anomaly-rca"},
     "chaos_genius.jobs.analytics_scheduler.*": {"queue": "anomaly-rca"},
     "chaos_genius.jobs.alert_tasks.*": {"queue": "alerts"},

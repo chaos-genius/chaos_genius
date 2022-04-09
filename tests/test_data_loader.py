3d9a77cb8b13b6f62496422ef001b747bd7e2633
Kartikay Bagla <kartikaybagla@gmail.com>
2022-04-09 15:19:18 +0530

    fix(data-loader): updated tests for iso8601 datetimes

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -42,16 +42,13 @@ def test_data_loader(monkeypatch: MonkeyPatch):
 
     # tail
     tail = 5
-    dl = data_loader.DataLoader(
-        kpi_info,
-        tail=tail
-    )
+    dl = data_loader.DataLoader(kpi_info, tail=tail)
     output_query = f"""select * from "cloud_cost" limit {tail}"""
     assert output_query == dl._build_query().strip()
 
     # count
     dl = data_loader.DataLoader(kpi_info)
-    output_query = 'select count(*) from "cloud_cost\"'
+    output_query = 'select count(*) from "cloud_cost"'
     assert output_query == dl._build_query(count=True).strip()
 
     # query
@@ -67,7 +64,9 @@ def test_data_loader(monkeypatch: MonkeyPatch):
     }
 
     dl = data_loader.DataLoader(kpi_info)
-    output_query = r"select \* from \(select \* from cloud_cost\) as \"[a-z]{10}\""
+    output_query = (
+        r"select \* from \(select \* from cloud_cost\) as \"[a-z]{10}\""
+    )
     assert re.match(output_query, dl._build_query().strip())
 
     # tests for different date inputs
@@ -84,86 +83,75 @@ def test_data_loader(monkeypatch: MonkeyPatch):
     }
 
     # no end_date, no start_date, no days_before
-    dl = data_loader.DataLoader(
-        kpi_info
-    )
+    dl = data_loader.DataLoader(kpi_info)
     output_query = """select * from "cloud_cost\""""
     assert output_query == dl._build_query().strip()
 
     # end_date, no start_date, no days_before
     end_date = date(2020, 1, 1)
-    dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date
-    )
+    dl = data_loader.DataLoader(kpi_info, end_date=end_date)
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00'"""
     assert output_query == dl._build_query().strip()
 
     # no end_date, start_date, no days_before
     start_date = date(2019, 1, 1)
-    dl = data_loader.DataLoader(
-        kpi_info,
-        start_date=start_date
-    )
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00'"""
+    dl = data_loader.DataLoader(kpi_info, start_date=start_date)
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00'"""
     assert output_query == dl._build_query().strip()
 
     # end_date, start_date, no days_before
     end_date = date(2020, 1, 1)
     start_date = date(2019, 1, 1)
     dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date,
-        start_date=start_date
+        kpi_info, end_date=end_date, start_date=start_date
     )
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00'"""
     assert output_query == dl._build_query().strip()
 
     # no end_date, no start_date, days_before
-    with pytest.raises(ValueError, match='If days_before is specified, either start_date or end_date must be specified'):
-        dl = data_loader.DataLoader(
-            kpi_info,
-            days_before=1
-        )
+    with pytest.raises(
+        ValueError,
+        match="If days_before is specified, either start_date or end_date must be specified",
+    ):
+        dl = data_loader.DataLoader(kpi_info, days_before=1)
 
     # end_date, no start_date, days_before
     end_date = date(2020, 1, 1)
     days_before = 30
     dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date,
-        days_before=days_before
+        kpi_info, end_date=end_date, days_before=days_before
     )
     start_date = end_date - timedelta(days=days_before)
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00'"""
     assert output_query == dl._build_query().strip()
 
     # no end_date, start_date, days_before
     start_date = date(2019, 1, 1)
     days_before = 30
     dl = data_loader.DataLoader(
-        kpi_info,
-        start_date=start_date,
-        days_before=days_before
+        kpi_info, start_date=start_date, days_before=days_before
     )
     end_date = start_date + timedelta(days=days_before)
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00'"""
     assert output_query == dl._build_query().strip()
 
     # end_date, start_date, days_before
     end_date = date(2020, 1, 1)
     start_date = date(2019, 1, 1)
     days_before = 30
-    with pytest.raises(ValueError, match='end_date, start_date and days_before cannot be specified at the same time'):
+    with pytest.raises(
+        ValueError,
+        match="end_date, start_date and days_before cannot be specified at the same time",
+    ):
         dl = data_loader.DataLoader(
             kpi_info,
             end_date=end_date,
             start_date=start_date,
-            days_before=days_before
+            days_before=days_before,
         )
 
     # tz naive testing
@@ -171,18 +159,16 @@ def test_data_loader(monkeypatch: MonkeyPatch):
     end_date = date(2020, 1, 1)
     start_date = date(2019, 1, 1)
     dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date,
-        start_date=start_date
+        kpi_info, end_date=end_date, start_date=start_date
     )
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 05:30:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 05:30:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T05:30:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T05:30:00'"""
     assert output_query == dl._build_query().strip()
 
     data_source["database_timezone"] = "America/New_York"
     start_date = start_date - timedelta(days=1)
     end_date = end_date - timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 20:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 20:00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T20:00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T20:00:00'"""
 
     # tz aware testing
     kpi_info["timezone_aware"] = True
@@ -190,23 +176,23 @@ def test_data_loader(monkeypatch: MonkeyPatch):
     end_date = date(2020, 1, 1)
     start_date = date(2019, 1, 1)
     dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date,
-        start_date=start_date
+        kpi_info, end_date=end_date, start_date=start_date
     )
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00 +00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00 +00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00+00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00+00:00'"""
     assert output_query == dl._build_query().strip()
 
     data_source["database_timezone"] = "America/New_York"
     start_date = start_date - timedelta(days=1)
     end_date = end_date - timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00 +00:00' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00 +00:00'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00+00:00' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00+00:00'"""
 
     from chaos_genius import settings
+
     settings.TIMEZONE = "Asia/Kolkata"
 
     import importlib
+
     importlib.reload(data_loader)
 
     kpi_info["timezone_aware"] = True
@@ -214,15 +200,13 @@ def test_data_loader(monkeypatch: MonkeyPatch):
     end_date = date(2020, 1, 1)
     start_date = date(2019, 1, 1)
     dl = data_loader.DataLoader(
-        kpi_info,
-        end_date=end_date,
-        start_date=start_date
+        kpi_info, end_date=end_date, start_date=start_date
     )
     end_date = end_date + timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00 +05:30' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00 +05:30'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00+05:30' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00+05:30'"""
     assert output_query == dl._build_query().strip()
 
     data_source["database_timezone"] = "America/New_York"
     start_date = start_date - timedelta(days=1)
     end_date = end_date - timedelta(days=1)
-    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")} 00:00:00 +05:30' and "date" < '{end_date.strftime("%Y-%m-%d")} 00:00:00 +05:30'"""
+    output_query = f"""select * from "cloud_cost" where "date" >= '{start_date.strftime("%Y-%m-%d")}T00:00:00+05:30' and "date" < '{end_date.strftime("%Y-%m-%d")}T00:00:00+05:30'"""

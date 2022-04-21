d4903b06aa6551ba1e5bb278524c3c7756867047
Samyak S Sarnayak <samyak201@gmail.com>
2022-04-21 12:50:27 +0530

    fix: change parameter name from query to search
    frontend requirement

--- chaos_genius/utils/search.py
+++ chaos_genius/utils/search.py
@@ -8,15 +8,15 @@ from flask.wrappers import Request
 
 
 def make_search_filter(req: Request, column):
-    """Creates a search filter on the given column from the `query` URL argument.
+    """Creates a search filter on the given column from the `search` URL argument.
 
     Note: to be used with a GET request since this works on the URL parameters.
 
     Uses `ILIKE` to search - does not support fuzzy matching.
 
     Returns:
-        An SQLAlchemy filter if the `query` argument was given, `None` otherwise.
+        An SQLAlchemy filter if the `search` argument was present, `None` otherwise.
     """
-    query = req.args.get("query")
+    query = req.args.get("search")
     if query is not None:
         return column.ilike(f"%{query}%")

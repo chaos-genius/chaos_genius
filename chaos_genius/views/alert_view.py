chaos-genius/chaos_genius

commit 79328ca66c79f5699b391ca0ffb6097a57aff0d8

fix: Return all data when empty filter is passed.
Made changes to return the filter params are empty.

Now multiple values for a filter param can be sent
spererated by '&' or the values can be ',' separated
or a combination of both.

chaos_genius/views/alert_view.py
@@ -36,14 +36,22 @@ def list_alert():
     filters = []
     if search_filter is not None:
         filters.append(search_filter)
-    if channels_to_filter:
-        filters.append(Alert.alert_channel.in_(channels_to_filter))
-    if actives_to_filter:
+    if channels_to_filter and channels_to_filter!=[""]:
+        filters.append(Alert.alert_channel.in_(
+                [
+                    channel_filter
+                    for channel_to_filter in channels_to_filter
+                    for channel_filter in channel_to_filter.split(",")
+                ]
+            )
+        )
+    if actives_to_filter and actives_to_filter!=[""]:
         filters.append(
             Alert.active.in_(
                 [
-                    active_to_filter in TRUE_VALUES
+                    active_filter in TRUE_VALUES
                     for active_to_filter in actives_to_filter
+                    for active_filter in active_to_filter.split(",")
                 ]
             )
         )

chaos_genius/views/data_source_view.py
@@ -100,8 +100,15 @@ def data_source():
         filters = [DataSource.active == True]  # noqa: E712
         if search_filter is not None:
             filters.append(search_filter)
-        if datasource_types:
-            filters.append(DataSource.connection_type.in_(datasource_types))
+        if datasource_types and datasource_types!=[""]:
+            filters.append(DataSource.connection_type.in_(
+                    [
+                        source_type
+                        for datasource_type in datasource_types
+                        for source_type in datasource_type.split(",")
+                    ]
+                )
+            )
 
         data_sources_paginated: Pagination = (
             DataSource.query.filter(*filters)

chaos_genius/views/kpi_view.py
@@ -117,7 +117,7 @@ def kpi():
         )
 
     elif request.method == "GET":
-        dashboard_ids = request.args.getlist("dashboard_id")
+        dashboard_ids_list = request.args.getlist("dashboard_id")
         datasource_types = request.args.getlist("datasource_type")
         page, per_page = pagination_args(request)
         search_query, search_filter = make_search_filter(request, Kpi.name)
@@ -125,11 +125,22 @@ def kpi():
         filters = [Kpi.active == True]  # noqa: E712
         if search_filter is not None:
             filters.append(search_filter)
-        if datasource_types:
-            filters.append(DataSource.connection_type.in_(datasource_types))
+        if datasource_types and datasource_types!=[""]:
+            filters.append(DataSource.connection_type.in_(
+                    [
+                        source_type
+                        for datasource_type in datasource_types
+                        for source_type in datasource_type.split(",")
+                    ]
+                )
+            )
 
-        if dashboard_ids:
-            dashboard_ids = [int(dashboard_id) for dashboard_id in dashboard_ids]
+        if dashboard_ids_list and dashboard_ids_list!=[""]:
+            dashboard_ids = [
+                                int(dashboard_id)
+                                for dashboard_ids in dashboard_ids_list
+                                for dashboard_id in dashboard_ids.split(",")
+                            ]
             kpis_paginated: Pagination = (
                 db.session.query(Kpi, DataSource)
                 .join(DataSource, Kpi.data_source == DataSource.id)

commit 1b010e1ec2b344e7cafcaf160b395bff1c617965

chore(api): fix formatting in filter changes
Inconsistencies were introduced in 79328ca66c79f5699b391ca0ffb6097a57aff0d8

chaos_genius/views/alert_view.py
@@ -36,8 +36,9 @@ def list_alert():
     filters = []
     if search_filter is not None:
         filters.append(search_filter)
-    if channels_to_filter and channels_to_filter!=[""]:
-        filters.append(Alert.alert_channel.in_(
+    if channels_to_filter and channels_to_filter != [""]:
+        filters.append(
+            Alert.alert_channel.in_(
                 [
                     channel_filter
                     for channel_to_filter in channels_to_filter
@@ -45,7 +46,7 @@ def list_alert():
                 ]
             )
         )
-    if actives_to_filter and actives_to_filter!=[""]:
+    if actives_to_filter and actives_to_filter != [""]:
         filters.append(
             Alert.active.in_(
                 [

chaos_genius/views/data_source_view.py
@@ -100,7 +100,7 @@ def data_source():
         filters = [DataSource.active == True]  # noqa: E712
         if search_filter is not None:
             filters.append(search_filter)
-        if datasource_types and datasource_types!=[""]:
+        if datasource_types and datasource_types != [""]:
             filters.append(DataSource.connection_type.in_(
                     [
                         source_type

chaos_genius/views/kpi_view.py
@@ -125,8 +125,9 @@ def kpi():
         filters = [Kpi.active == True]  # noqa: E712
         if search_filter is not None:
             filters.append(search_filter)
-        if datasource_types and datasource_types!=[""]:
-            filters.append(DataSource.connection_type.in_(
+        if datasource_types and datasource_types != [""]:
+            filters.append(
+                DataSource.connection_type.in_(
                     [
                         source_type
                         for datasource_type in datasource_types
@@ -135,12 +136,12 @@ def kpi():
                 )
             )
 
-        if dashboard_ids_list and dashboard_ids_list!=[""]:
+        if dashboard_ids_list and dashboard_ids_list != [""]:
             dashboard_ids = [
-                                int(dashboard_id)
-                                for dashboard_ids in dashboard_ids_list
-                                for dashboard_id in dashboard_ids.split(",")
-                            ]
+                int(dashboard_id)
+                for dashboard_ids in dashboard_ids_list
+                for dashboard_id in dashboard_ids.split(",")
+            ]
             kpis_paginated: Pagination = (
                 db.session.query(Kpi, DataSource)
                 .join(DataSource, Kpi.data_source == DataSource.id)
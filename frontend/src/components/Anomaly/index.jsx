58d83a78be300632e44be938e02acbbf4c40f2ca
chartisdev <db4friends@gmail.com>
2022-06-20 20:00:23 +0530

    removed retrain model when both dimension and value selected

--- frontend/src/components/Anomaly/index.jsx
+++ frontend/src/components/Anomaly/index.jsx
@@ -614,14 +614,16 @@ const Anomaly = ({ kpi, anomalystatus, dashboard }) => {
                     />
                   )}
 
-                  <div className="retrain-button-container">
-                    <div
-                      className="retrain-button"
-                      onClick={() => handleRetrain()}>
-                      <img src={Refresh} alt="alert-notification" />{' '}
-                      <span>Retrain Model</span>
+                  {(!dimension || !value) && (
+                    <div className="retrain-button-container">
+                      <div
+                        className="retrain-button"
+                        onClick={() => handleRetrain()}>
+                        <img src={Refresh} alt="alert-notification" />{' '}
+                        <span>Retrain Model</span>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
 

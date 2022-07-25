ac8325f5d47b07bbc11a8fe0a40a9a8f29c50da6
Kartikay Bagla <kartikaybagla@gmail.com>
2022-07-25 13:47:39 +0530

    feat: added ENV variable, non-json log messages for development

--- chaos_genius/logger.py
+++ chaos_genius/logger.py
@@ -1,42 +1,44 @@
 """Provides utility functions for logging."""
 import logging
 from logging.handlers import RotatingFileHandler
+
 from pythonjsonlogger import jsonlogger
 
-from chaos_genius.utils.sentry import init_sentry, log_sentry_error
 from chaos_genius.settings import ENV, SENTRY_DSN
+from chaos_genius.utils.sentry import init_sentry, log_sentry_error
 
 
 def configure_logger(app):
     """Configure loggers."""
     logger = logging.getLogger("chaos_genius")
-    json_handler = RotatingFileHandler(
+    rf_handler = RotatingFileHandler(
         "chaosgenius.log", maxBytes=4194304, backupCount=5
     )
-    json_formatter = jsonlogger.JsonFormatter(
-        "%(asctime)s %(levelname)s %(name)s "
-        "%(message)s %(lineno)s %(funcName)s %(filename)s"
-    )
-    json_handler.setFormatter(json_formatter)
-    streamHandler = logging.StreamHandler()
+    stream_handler = logging.StreamHandler()
+
+    format_string = "%(asctime)s %(levelname)s %(name)s %(message)s" \
+        + "%(lineno)s %(funcName)s %(filename)s"
 
     if ENV == "production":
-        streamHandler.setFormatter(json_formatter)
+        formatter = jsonlogger.JsonFormatter(format_string)
         log_level = logging.INFO
         if SENTRY_DSN:
             init_sentry(SENTRY_DSN, app, ENV)
     else:
+        formatter = logging.Formatter(format_string)
         log_level = logging.DEBUG
 
-    # log_level = logging.INFO if ENV == "production" else logging.DEBUG
+    rf_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+
     logger.setLevel(log_level)
 
-    logger.addHandler(json_handler)
-    logger.addHandler(streamHandler)
+    logger.addHandler(rf_handler)
+    logger.addHandler(stream_handler)
 
     if not app.logger.handlers:
-        app.logger.addHandler(json_handler)
-        app.logger.addHandler(streamHandler)
+        app.logger.addHandler(rf_handler)
+        app.logger.addHandler(stream_handler)
 
 
 def log_error(error):

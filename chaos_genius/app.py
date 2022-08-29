13e58dc3675c1a6244d2b15e07bac708b855a57d
Samyak S Sarnayak <samyak201@gmail.com>
2022-08-29 16:36:19 +0530

    fix(demo): use a single level before_request hook
    Multi-level/nested was unnecessary and gave an error in debug mode:
    https://stackoverflow.com/questions/66190958/python-flask-framework-assertionerror-a-setup-function-was-called-after-the-fi

--- chaos_genius/app.py
+++ chaos_genius/app.py
@@ -136,26 +136,23 @@ def register_hooks(app):
     def demo_endpoints_blocker():
         """Hook to allow access to only whitelisted APIs in demo."""
         if REACT_APP_IS_DEMO:
-
-            @app.before_request
-            def before_request():
-                if request.endpoint not in DEMO_ENDPOINT_WHITELIST:
+            if request.endpoint not in DEMO_ENDPOINT_WHITELIST:
+                return jsonify(
+                    {
+                        "status": "failure",
+                        "message": "Endpoint not accessible in demo version",
+                    }
+                )
+            else:
+                if request.method not in DEMO_ENDPOINT_WHITELIST[request.endpoint]:
                     return jsonify(
                         {
                             "status": "failure",
-                            "message": "Endpoint not accessible in demo version",
+                            "message": f"{request.method} method is not allowed for this endpoint in demo version",
                         }
                     )
                 else:
-                    if request.method not in DEMO_ENDPOINT_WHITELIST[request.endpoint]:
-                        return jsonify(
-                            {
-                                "status": "failure",
-                                "message": f"{request.method} method is not allowed for this endpoint in demo version",
-                            }
-                        )
-                    else:
-                        pass
+                    pass
 
 
 def register_commands(app):
